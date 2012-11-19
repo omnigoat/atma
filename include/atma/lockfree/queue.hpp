63e8cabc4017256271b7858dbbef0f42a464e0dc
omnigoat <379771+omnigoat@users.noreply.github.com>
2012-11-19 11:32:53 +1100

    began cleaning up of queue.hpp

--- include/atma/lockfree/queue.hpp
+++ include/atma/lockfree/queue.hpp
@@ -11,70 +11,26 @@ namespace lockfree {
 	// cache padding defined as 4kb
 	const unsigned int cache_line_size = 4 * 1024;
 
-	namespace detail {
-		template <typename T>
-		struct queue_node_t
-		{
-			queue_node_t() : value(nullptr), next(nullptr) {}
-			queue_node_t(const T& value) : value(new T(value)), next(nullptr) {}
-			~queue_node_t() { delete value; }
 
-			T* value;
-			std::atomic<queue_node_t*> next;
-			char pad[ cache_line_size - sizeof(T*) - sizeof(std::atomic<queue_node_t*>) ];
-		};
-	}
-
+	//=====================================================================
+	// queue_t
+	//=====================================================================
 	template <typename T>
 	struct queue_t
 	{
-		typedef detail::queue_node_t<T> node_t;
+		queue_t();
+		~queue_t();
 
-		queue_t() {
-			head_ = tail_ = new node_t;
-			producer_lock_ = consumer_lock_ = false;
-		};
+		void push_back(const T& t);
+		bool pop(T& result);
+		
+	private:
+		queue_t(const queue_t&);
+		queue_t(queue_t&&);
+		queue_t& operator = (const queue_t&);
+		queue_t& operator = (queue_t&&);
 
-		~queue_t() {
-			while (head_ != nullptr) {
-				node_t* tmp = head_;
-				head_ = tmp->next;
-				delete tmp;
-			}
-		}
-
-		void push_back(const T& t) {
-			node_t* tmp = new node_t(t);
-			while (producer_lock_.exchange(true))
-				;
-			
-			tail_->next = tmp;
-			tail_ = tmp;
-			producer_lock_ = false;
-		}
-
-		bool pop(T& result)
-		{
-			while (consumer_lock_.exchange(true))
-				;
-
-			node_t* head = head_;
-			node_t* head_next = head_->next;
-			if (head_next)
-			{
-				T* value = head_next->value;
-				head_next->value = nullptr;
-				head_ = head_next;
-				consumer_lock_ = false;
-
-				result = *value;
-				delete head;
-				return true;
-			}
-
-			consumer_lock_ = false;
-			return false;
-		}
+		struct node_t;
 
 	private:
 		char pad0[cache_line_size];
@@ -92,6 +48,82 @@ namespace lockfree {
 		char pad4[cache_line_size - sizeof(std::atomic_bool)];
 	};
 	
+
+	//=====================================================================
+	// queue_t::node_t
+	//=====================================================================
+	template <typename T>
+	struct queue_t<T>::node_t
+	{
+		node_t() : value(nullptr), next(nullptr) {}
+		node_t(const T& value) : value(new T(value)), next(nullptr) {}
+		~node_t() { delete value; }
+
+		T* value;
+		std::atomic<node_t*> next;
+		char pad[ cache_line_size - sizeof(T*) - sizeof(std::atomic<node_t*>) ];
+	};
+
+
+
+
+
+
+	//=====================================================================
+	// queue_t implementation
+	//=====================================================================
+	template <typename T>
+	queue_t<T>::queue_t() {
+		head_ = tail_ = new node_t;
+		producer_lock_ = consumer_lock_ = false;
+	};
+
+	template <typename T>
+	queue_t<T>::~queue_t() {
+		while (head_ != nullptr) {
+			node_t* tmp = head_;
+			head_ = tmp->next;
+			delete tmp;
+		}
+	}
+
+	template <typename T>
+	void queue_t<T>::push_back(const T& t) {
+		node_t* tmp = new node_t(t);
+		while (producer_lock_.exchange(true))
+			;
+			
+		tail_->next = tmp;
+		tail_ = tmp;
+		producer_lock_ = false;
+	}
+
+	template <typename T>
+	bool queue_t<T>::pop(T& result)
+	{
+		while (consumer_lock_.exchange(true))
+			;
+
+		node_t* head = head_;
+		node_t* head_next = head_->next;
+		if (head_next)
+		{
+			T* value = head_next->value;
+			head_next->value = nullptr;
+			head_ = head_next;
+			consumer_lock_ = false;
+
+			result = *value;
+			delete head;
+			return true;
+		}
+
+		consumer_lock_ = false;
+		return false;
+	}
+
+
+
 //=====================================================================
 } // namespace lockfree
 } // namespace atma
