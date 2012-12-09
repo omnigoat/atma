4f131aea8c73e72056eb482e63f956300f79064c
omnigoat <379771+omnigoat@users.noreply.github.com>
2012-12-10 02:41:38 +1100

    moved atma/math to atma/math_old, and began work on new expression template library

--- include/atma/math/vector.hpp
+++ include/atma/math/vector.hpp
@@ -1,394 +1,261 @@
 //=====================================================================
 //
-//  vector
-//  ---------
-//    This is a templated 3D vector class. It's designed to be
-//    specialised if required, but that's not necessary (say, if you
-//    have need for a 72 dimension vector). It takes the two template
-//    arguments of E and T, where E is the number of elements, and T
-//    is the type of those elements.
-//
-//    As it stands, g++ can't handle having the definitions of the
-//    methods in the class itself, so we only declare them in the class
-//    and define them later on. Further down the file, we have a series
-//    of helpful vector functions. These do not need to be specialised
-//    for specialised versions of vector.
-//
 //=====================================================================
 #ifndef ATMA_MATH_VECTOR_HPP
 #define ATMA_MATH_VECTOR_HPP
 //=====================================================================
-#include <cmath>
+namespace atma {
 //=====================================================================
-#include "math__core.hpp"
-#include "et/expr.hpp"
-#include "et/binary_expr.hpp"
-#include "vector2.hpp"
-#include "vector3.hpp"
-#include "vector4.hpp"
+template <unsigned int E, typename T> struct vector;
 //=====================================================================
-ATMA_MATH_BEGIN
-//=====================================================================
+	template <class OP, class EXPR>
+	struct expr;
 
-	//=====================================================================
-	// Vector of E dimensions of type T
-	//=====================================================================
-	template <size_t E, typename T = float> struct vector
+	template <typename T>
+	struct vector<3, T>
 	{
-		typedef float length_type;
+		// constructors
+		vector();
+		vector(const T& x, const T& y, const T& z);
+		vector(const vector& rhs);
 		
-		//=====================================================================
-		// Constructor: Default
-		//=====================================================================
-		vector()
-		 : m_elements()
-		{
+		vector& operator = (const vector& rhs);
+
+		const T& operator [] (int i) const {
+			return *(&x_ + i);
 		}
-		
-		//=====================================================================
-		// Access
-		//=====================================================================
-		inline T& operator ()(size_t i)
-		{
-			return m_elements[i];
-		}
-		
-		inline const T& operator ()(size_t i) const
-		{
-			return m_elements[i];
-		}
-		
-		//=====================================================================
-		// Self-Addition
-		//=====================================================================
-		inline vector<E, T>& operator += (const vector<E, T>& rhs)
-		{
-			for (size_t i = 0; i < E; ++i) (*this)(i) += rhs(i);
-			return *this;
-		}
-		
-		//=====================================================================
-		// Self-Subration
-		//=====================================================================
-		inline vector<E, T>& operator -= (const vector<E, T>& rhs)
-		{
-			for (size_t i = 0; i < E; ++i) (*this)(i) -= rhs(i);
-			return *this;
-		}
-		
-		//=====================================================================
-		// Self-Multiplication
-		//=====================================================================
-		template <typename Y> vector<E, T>& operator *= (const Y& rhs)
-		{
-			for (size_t i = 0; i < E; ++i) (*this)(i) *= rhs;
-			return *this;
-		}
-		
-		//=====================================================================
-		// Self-Division
-		//=====================================================================
-		template <typename Y> vector<E, T>& operator /= (const Y& rhs)
-		{
-			for (size_t i = 0; i < E; ++i) (*this)(i) /= rhs;
+
+		vector& operator += (const vector& rhs);
+		vector& operator -= (const vector& rhs);
+		vector& operator *= (const T& rhs);
+		vector& operator /= (const T& rhs);
+
+		template <class OP, class EXPR>
+		vector& operator = (const expr<OP, EXPR>& e) {
+			x_ = e[0];
+			y_ = e[1];
+			z_ = e[2];
 			return *this;
 		}
 
-	protected:
-		// the elements in our vector
-		T m_elements[E];
+		const T& x() const { return x_; }
+		const T& y() const { return y_; }
+		const T& z() const { return z_; }
+
+		vector& set(const T& x, const T& y, const T& z);
+		vector& set_x(const T& x);
+		vector& set_y(const T& y);
+		vector& set_z(const T& z);
+
+		
+	private:
+		T x_, y_, z_;
 	};
-	
-	
+
 
 	//=====================================================================
-	//
-	//                S T A N D A R D    O P E R A T O R S
-	//              ----------------------------------------
-	//
-	//  These operators are pretty standard for vectors, so we allow them
-	//  to be only written once (here), and used for a vector of any size.
-	//
+	// implementation
 	//=====================================================================
+	template <typename T>
+	vector<3, T>::vector()
+	 : x_(), y_(), z_()
+	{
+	}
+
+	template <typename T>
+	vector<3, T>::vector(const T& x, const T& y, const T& z)
+	 : x_(x), y_(y), z_(z)
+	{
+	}
+
+	template <typename T>
+	vector<3, T>::vector(const vector<3, T>& rhs)
+	 : x_(rhs.x), y_(rhs.y), z_(rhs.z)
+	{
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::operator = (const vector<3, T>& rhs) {
+		x_ = rhs.x_;
+		y_ = rhs.y_;
+		z_ = rhs.z_;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::operator += (const vector<3, T>& rhs) {
+		x_ += rhs.x_;
+		y_ += rhs.y_;
+		z_ += rhs.z_;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::operator -= (const vector<3, T>& rhs) {
+		x_ -= rhs.x_;
+		y_ -= rhs.y_;
+		z_ -= rhs.z_;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::operator *= (const T& rhs) {
+		x_ *= rhs;
+		y_ *= rhs;
+		z_ *= rhs;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::operator /= (const T& rhs) {
+		x_ /= rhs;
+		y_ /= rhs;
+		z_ /= rhs;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::set(const T& x, const T& y, const T& z)
+	{
+		x_ = x;
+		y_ = y;
+		z_ = z;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::set_x(const T& x)
+	{
+		x_ = x;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::set_y(const T& y)
+	{
+		y_ = y;
+		return *this;
+	}
+
+	template <typename T>
+	vector<3, T>& vector<3, T>::set_z(const T& z)
+	{
+		z_ = z;
+		return *this;
+	}
+
+
+
+
+
+
+
+
+	template <typename T, typename R = T, bool is_integral = std::is_integral<T>::value>
+	struct value {
+		static R get(const T& in, unsigned int i) {
+			return in[i];
+		}
+	};
+
+	template <typename T, typename R>
+	struct value<T, R, true> {
+		static R get(const T& in, unsigned int) {
+			return in;
+		}
+	};
+
+	#define ATMA_MATH_BINARY_OPER(NAME, OP) \
+	template <typename R> \
+	struct NAME { \
+		typedef R result_type; \
+		static R result(const R& lhs, const R& rhs) { \
+			return lhs OP rhs; \
+		} \
+	};
+
+	ATMA_MATH_BINARY_OPER(add_oper, +)
+	ATMA_MATH_BINARY_OPER(sub_oper, -)
+	ATMA_MATH_BINARY_OPER(mul_oper, *)
+	ATMA_MATH_BINARY_OPER(div_oper, /)
+
+	// expr
+	template <class OP, class EXPR>
+	struct expr;
+	
+	// binary_expr
+	template <typename LHS, typename RHS>
+	struct binary_expr;
+
+	// expr again
+	template <class OP, typename LHS, typename RHS>
+	struct expr<OP, binary_expr<LHS, RHS>>
+	{
+		expr(const LHS& lhs, const RHS& rhs)
+		 : lhs(lhs), rhs(rhs)
+		  {}
+
+		typename OP::result_type operator [](int i) const {
+			return OP::result(
+				value<LHS, typename OP::result_type>::get(lhs, i),
+				value<RHS, typename OP::result_type>::get(rhs, i)
+			);
+		}
+	private:
+		const LHS& lhs;
+		const RHS& rhs;
+	};
+
+	
+
+
+
+	typedef vector<3, float> vector3f;
+
+	expr<add_oper<float>, binary_expr<vector3f, vector3f>> operator + (const vector3f& lhs, const vector3f& rhs) {
+		return expr<add_oper<float>, binary_expr<vector3f, vector3f>>(lhs, rhs);
+	}
+
+	expr<sub_oper<float>, binary_expr<vector3f, vector3f>> operator - (const vector3f& lhs, const vector3f& rhs) {
+		return expr<sub_oper<float>, binary_expr<vector3f, vector3f>>(lhs, rhs);
+	}
+
+	template <typename T, template <typename> class OP, class EXPR>
+	auto operator - (const vector<3, T>& lhs, const expr<OP<T>, EXPR>& rhs)
+	-> expr<sub_oper<T>, binary_expr<vector<3, T>, expr<OP<T>, EXPR>>> {
+		return expr<sub_oper<T>, binary_expr<vector<3, T>, expr<OP<T>, EXPR>>>(lhs, rhs);
+	}
+
 	/*
-	//========================================================================
-	// Addition
-	//========================================================================
-	template <size_t E, typename T>
-	et::expr< et::binary_expr<vector<E, T>, vector<E, T>, et::add_oper> > operator +
-		(const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::add_oper>(lhs, rhs) );
+	binary_expr<sub_oper<float>, vector3f, vector3f> operator - (const vector3f& lhs, const vector3f& rhs) {
+		return binary_expr<sub_oper<float>, vector3f, vector3f>(lhs, rhs);
 	}
 
-	template <size_t E, typename T, typename LHS_EXPR>
-	et::expr< et::binary_expr<et::expr<LHS_EXPR>, vector<E, T>, et::add_oper> > operator +
-		(const et::expr<LHS_EXPR>& lhs, const vector<E, T>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::add_oper>(lhs, rhs) );
-	}
-
-	template <size_t E, typename T, typename RHS_EXPR>
-	et::expr< et::binary_expr<vector<E, T>, et::expr<RHS_EXPR>, et::add_oper> > operator +
-		(const vector<E, T>& lhs, const et::expr<RHS_EXPR>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::add_oper>(lhs, rhs) );
-	}
-
-	template <typename LHS_EXPR, typename RHS_EXPR>
-	et::expr< et::binary_expr<et::expr<LHS_EXPR>, et::expr<RHS_EXPR>, et::add_oper> > operator + 
-		(const et::expr<LHS_EXPR>& lhs, const et::expr<RHS_EXPR>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::add_oper>(lhs, rhs) );
-	}
-
-
-	//========================================================================
-	// Subtraction
-	//========================================================================
-	template <size_t E, typename T>
-	et::expr< et::binary_expr<vector<E, T>, vector<E, T>, et::sub_oper> > operator -
-		(const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::sub_oper>(lhs, rhs) );
-	}
-
-	template <size_t E, typename T, typename LHS_EXPR>
-	et::expr< et::binary_expr<et::expr<LHS_EXPR>, vector<E, T>, et::sub_oper> > operator -
-		(const et::expr<LHS_EXPR>& lhs, const vector<E, T>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::sub_oper>(lhs, rhs) );
-	}
-
-	template <size_t E, typename T, typename RHS_EXPR>
-	et::expr< et::binary_expr<vector<E, T>, et::expr<RHS_EXPR>, et::sub_oper> > operator -
-		(const vector<E, T>& lhs, const et::expr<RHS_EXPR>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::sub_oper>(lhs, rhs) );
-	}
-
-	template <typename LHS_EXPR, typename RHS_EXPR>
-	et::expr< et::binary_expr<et::expr<LHS_EXPR>, et::expr<RHS_EXPR>, et::sub_oper> > operator - 
-		(const et::expr<LHS_EXPR>& lhs, const et::expr<RHS_EXPR>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::sub_oper>(lhs, rhs) );
-	}
-
-
-	//========================================================================
-	// Multiplication
-	//========================================================================
-	template <size_t E, typename T>
-	et::expr< et::binary_expr<vector<E, T>, T, et::mult_oper> > operator *
-		(const vector<E, T>& lhs, const T& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::mult_oper>(lhs, rhs) );
+	template <template <typename> class OP, typename R_LHS, typename R_RHS>
+	binary_expr<sub_oper<float>, vector3f, binary_expr<OP<float>, R_LHS, R_RHS>>
+	operator - (const vector3f& lhs, const binary_expr<OP<float>, R_LHS, R_RHS>& rhs) {
+		return binary_expr<sub_oper<float>, vector3f, binary_expr<OP<float>, R_LHS, R_RHS>>(lhs, rhs);
 	}
 	
-	template <size_t E, typename T>
-	et::expr< et::binary_expr<T, vector<E, T>, et::mult_oper> > operator *
-		(const T& lhs, const vector<E, T>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::mult_oper>(lhs, rhs) );
+	// subtraction between two binary expressions
+	template <
+	 template <typename> class L_OP, typename L_LHS, typename L_RHS,
+	 template <typename> class R_OP, typename R_LHS, typename R_RHS
+	>
+	binary_expr<
+	 sub_oper<float>,
+	 binary_expr<L_OP<float>, L_LHS, L_RHS>,
+	 binary_expr<R_OP<float>, R_LHS, R_RHS>
+	>
+	operator - (const binary_expr<L_OP<float>, L_LHS, L_RHS>& lhs, const binary_expr<R_OP<float>, R_LHS, R_RHS>& rhs) {
+		return binary_expr<sub_oper<float>, binary_expr<L_OP<float>, L_LHS, L_RHS>, binary_expr<R_OP<float>, R_LHS, R_RHS>>(lhs, rhs);
 	}
 
-	template <typename T, typename LHS_EXPR>
-	et::expr< et::binary_expr<et::expr<LHS_EXPR>, T, et::mult_oper> > operator *
-		(const et::expr<LHS_EXPR>& lhs, const float& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::mult_oper>(lhs, rhs) );
-	}
-
-	template <typename RHS_EXPR>
-	et::expr< et::binary_expr<float, et::expr<RHS_EXPR>, et::mult_oper> > operator *
-		(const float& lhs, const et::expr<RHS_EXPR>& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::mult_oper>(lhs, rhs) );
-	}
-
-
-	//========================================================================
-	// Division
-	//========================================================================
-	template <size_t E, typename T>
-	et::expr< et::binary_expr< math::vector<E, T>, T, et::div_oper> > operator /
-		(const math::vector<E, T>& lhs, const T& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::div_oper>(lhs, rhs) );
-	}
-
-	template <typename LHS_EXPR>
-	et::expr< et::binary_expr<et::expr<LHS_EXPR>, float, et::div_oper> > operator /
-		(const et::expr<LHS_EXPR>& lhs, const float& rhs)
-	{
-		return et::make_expr( et::make_binary_expr<et::div_oper>(lhs, rhs) );
-	}
-
-
-
-	
+	expr<sub_oper<float>, binary_expr<vector3f, vector3f>>
 	*/
-	
-	
-	/*
-	//=====================================================================
-	// Addition: Vector
-	//=====================================================================
-	template <size_t E, typename T>
-	inline const vector<E, T> operator + (const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		vector<E, T> v = lhs;
-		v += rhs;
-		return v;
-	}
-	
-	//=====================================================================
-	// Subraction: Vector
-	//=====================================================================
-	template <size_t E, typename T>
-	inline const vector<E, T> operator - (const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		vector<E, T> v = lhs;
-		v -= rhs;
-		return v;
-	}
 
-	//=====================================================================
-	// Multiplication: Scalar
-	//=====================================================================
-	template <size_t E, typename T, typename Y> 
-	const vector<E, T> operator * (const vector<E, T>& lhs, const Y& rhs)
-	{
-		vector<E, T> v = lhs;
-		v *= rhs;
-		return v;
-	}
-	
-	template <size_t E, typename T, typename Y>
-	const vector<E, T> operator * (const Y& lhs, const vector<E, T>& rhs)
-	{
-		vector<E, T> v = rhs;
-		v *= lhs;
-		return v;
-	}
-
-	//=====================================================================
-	// Division: Scalar
-	//=====================================================================
-	template <size_t E, typename T, typename Y>
-	const vector<E, T> operator / (const vector<E, T>& lhs, const Y& rhs)
-	{
-		vector<E, T> v = lhs;
-		v /= rhs;
-		return v;
-	}
-	
-	template <size_t E, typename T, typename Y>
-	const vector<E, T> operator / (const Y& lhs, const vector<E, T>& rhs)
-	{
-		vector<E, T> v = rhs;
-		v /= lhs;
-		return v;
-	}
-*/
-	//=====================================================================
-	// Equality
-	//=====================================================================
-	template <size_t E, typename T>
-	inline bool operator == (const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		for (size_t i = 0; i < E; ++i)
-		{
-			if (lhs(i) != rhs(i)) return false;
-		}
-		return true;
-	}
-	
-	//=====================================================================
-	// Inequality
-	//=====================================================================
-	template <size_t E, typename T>
-	inline bool operator != (const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		for (size_t i = 0; i < E; ++i)
-		{
-			if (lhs(i) != rhs(i)) return true;
-		}
-		return false;
-	}
-
-
-
-
-	//=====================================================================
-	//
-	//               H E L P F U L     F U N C T I O N S
-	//             ---------------------------------------
-	//
-	//  These functions here operate on any vector and are the usual
-	//  suspects for 3D graphical workings. They rely on the implementation
-	//  used for the vectors.
-	//
-	//=====================================================================
-
-	//=====================================================================
-	// Dot-product
-	//=====================================================================
-	template <size_t E, typename T>
-	float DotProduct(const vector<E, T>& v1, const vector<E, T>& v2)
-	{
-		float result = 0;
-		for (size_t i = 0; i < E; ++i) result += float(v1(i) * v2(i));
-		return result;
-	}
-	
-	//=====================================================================
-	// Normalisation
-	//=====================================================================
-	template <size_t E, typename T>
-	vector<E, T> Normalize(const vector<E, T>& v)
-	{
-		float m = Magnitude(v);
-		if (m == 1) return v;
-		return v / m;
-	}
-	
-	//=====================================================================
-	// Magnitude
-	//=====================================================================
-	template <size_t E, typename T>
-	float Magnitude(const vector<E, T>& v1)
-	{
-		float f = 0;
-		for (size_t i = 0; i < E; ++i) f += v1(i) * v1(i);
-		return std::sqrt(f);
-	}
-	
-	//=====================================================================
-	// Returns the angle between two vectors
-	//=====================================================================
-	template <size_t E, typename T>
-	float Angle(const vector<E, T>& lhs, const vector<E, T>& rhs)
-	{
-		float m = Magnitude(lhs) * Magnitude(rhs);
-		if (m == 0.0f) return 0.0f;
-		return acos(DotProduct(lhs, rhs) / m);
-	}
-	
-	//=====================================================================
-	// Returns the midpoint between two vectors
-	//=====================================================================
-	template <size_t E, typename T>
-	vector<E, T> Midpoint(const vector<E, T>& v1, const vector<E, T>& v2)
-	{
-		return (v1 + v2) * 0.5f;
-	}
-	
-	
-	
-	
 //=====================================================================
-ATMA_MATH_CLOSE
+} // namespace atma
 //=====================================================================
 #endif
 //=====================================================================
