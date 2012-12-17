c7efdc4f6b53cba1dd37c3fcf02916a3a7e8e559
jona <omnigoat@gmail.com>
2012-12-18 08:23:07 +1100

    experimental binary_oper beginnings

--- include/atma/math/expr_tmpl/binary_operator.hpp
+++ include/atma/math/expr_tmpl/binary_operator.hpp
@@ -1,8 +1,8 @@
 //=====================================================================
 //
 //=====================================================================
-#ifndef ATMA_MATH_EXPR_TMPL_EXPRESSION_HPP
-#define ATMA_MATH_EXPR_TMPL_EXPRESSION_HPP
+#ifndef ATMA_MATH_EXPR_TMPL_BINARY_OPERATOR_HPP
+#define ATMA_MATH_EXPR_TMPL_BINARY_OPERATOR_HPP
 //=====================================================================
 #include <atma/math/expr_tmpl/utility.hpp>
 //=====================================================================
@@ -10,21 +10,31 @@ namespace atma {
 namespace expr_tmpl {
 //=====================================================================
 
-	template <typename R, typename OPER>
-	struct expr
+	template <template <typename, typename> class TAG, typename LHS, typename RHS>
+	struct binary_oper
 	{
-		expr(const OPER& oper)
-		 : oper(oper)
+		binary_oper(const LHS& lhs, const RHS& rhs)
+		 : lhs_(lhs), rhs_(rhs)
 		  {}
- 
-		typename element_type_of<OPER>::type
+		
+		typename element_type_of<TAG<LHS, RHS>>::type
 		 operator [](int i) const
-		  { return oper[i]; }
+		  { return tag_(lhs_, rhs_, i); }
 
 	private:
-		OPER oper;
+		TAG<LHS, RHS> tag_;
+		const LHS& lhs_;
+		const RHS& rhs_;
 	};
 
+
+	template <typename LHS, typename RHS>
+	struct add_oper {
+		typedef decltype(std::declval<LHS>()[0] + std::declval<RHS>()[0]) result_type;
+
+		result_type operator ()(const LHS& lhs, const RHS& rhs, int i) const
+		 { return lhs[i] + rhs[i]; }
+	};
 
 //=====================================================================
 } // namespace expr_tmpl
