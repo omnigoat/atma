d1ad75eb7526901cb9aaabdbc725b19716129e3f
jona <omnigoat@gmail.com>
2012-12-28 18:02:28 +1100

    updating vector, no specialisation for vector<3, T>, better binary_oper (fns don't take template arguments)

--- include/atma/math/vector.hpp
+++ include/atma/math/vector.hpp
@@ -4,33 +4,36 @@
 #ifndef ATMA_MATH_VECTOR_HPP
 #define ATMA_MATH_VECTOR_HPP
 //=====================================================================
+#include <array>
+#include <numeric>
 #include <type_traits>
+#include <initializer_list>
+#include <atma/assert.hpp>
 #include <atma/math/expr_tmpl/expression.hpp>
-#include <atma/math/expr_tmpl/elementwise_opers.hpp>
+#include <atma/math/expr_tmpl/elementwise_fns.hpp>
 #include <atma/math/expr_tmpl/operators.hpp>
 #include <atma/math/expr_tmpl/binary_operator.hpp>
 //=====================================================================
 namespace atma {
 //=====================================================================
-template <unsigned int E, typename T> struct vector;
-//=====================================================================
 	
-
-	template <typename T>
-	struct vector<3, T>
+	template <unsigned int E, typename T>
+	struct vector
 	{
 		typedef T element_type;
 
 		// constructors
 		vector();
-		vector(const T& x, const T& y, const T& z);
+		vector(std::initializer_list<T>);
+
 		vector(const vector& rhs);
 		
 		vector& operator = (const vector& rhs);
 
-		const T& operator [] (int i) const {
-			return *(&x_ + i);
-		}
+		const T& operator [] (int i) const;
+
+		const T* begin() const { return &*elements_.begin(); }
+		const T* end() const { return &*elements_.end(); }
 
 		vector& operator += (const vector& rhs);
 		vector& operator -= (const vector& rhs);
@@ -38,135 +41,154 @@ template <unsigned int E, typename T> struct vector;
 		vector& operator /= (const T& rhs);
 
 		template <class OP>
-		vector& operator = (const expr_tmpl::expr<vector<3, T>, OP>& e) {
-			x_ = e[0];
-			y_ = e[1];
-			z_ = e[2];
+		vector& operator = (const expr_tmpl::expr<vector<E, T>, OP>& e) {
+			for (auto i = 0u; i != E; ++i) {
+				elements_[i] = e[i];
+			}
 			return *this;
 		}
 
-		const T& x() const { return x_; }
-		const T& y() const { return y_; }
-		const T& z() const { return z_; }
+		vector& set(unsigned int i, const T& n);
 
-		vector& set(const T& x, const T& y, const T& z);
-		vector& set_x(const T& x);
-		vector& set_y(const T& y);
-		vector& set_z(const T& z);
-
-		
 	private:
-		T x_, y_, z_;
+		std::array<T, E> elements_;
 	};
 
 
 	//=====================================================================
 	// implementation
 	//=====================================================================
-	template <typename T>
-	vector<3, T>::vector()
-	 : x_(), y_(), z_()
+	template <unsigned int E, typename T>
+	vector<E, T>::vector()
 	{
 	}
 
-	template <typename T>
-	vector<3, T>::vector(const T& x, const T& y, const T& z)
-	 : x_(x), y_(y), z_(z)
+	template <unsigned int E, typename T>
+	vector<E, T>::vector(std::initializer_list<T> elements)
+	{
+		ATMA_ASSERT(elements.size() == E);
+		std::copy(elements.begin(), elements.end(), elements_.begin());
+	}
+
+	template <unsigned int E, typename T>
+	vector<E, T>::vector(const vector<E, T>& rhs)
+	 : elements_(rhs.elements_)
 	{
 	}
 
-	template <typename T>
-	vector<3, T>::vector(const vector<3, T>& rhs)
-	 : x_(rhs.x), y_(rhs.y), z_(rhs.z)
+	template <unsigned int E, typename T>
+	vector<E, T>& vector<E, T>::operator = (const vector<E, T>& rhs) {
+		elements_ = rhs.elements_;
+		return *this;
+	}
+
+	template <unsigned int E, typename T>
+	const T& vector<E, T>::operator [](int i) const {
+		return elements_[i];
+	}
+
+	template <unsigned int E, typename T>
+	vector<E, T>& vector<E, T>::operator += (const vector<E, T>& rhs) {
+		for (auto i = 0u; i != E; ++i)
+			elements_[i] += rhs.elements_[i];
+		return *this;
+	}
+
+	template <unsigned int E, typename T>
+	vector<E, T>& vector<E, T>::operator -= (const vector<E, T>& rhs) {
+		for (auto i = 0u; i != E; ++i)
+			elements_[i] -= rhs.elements_[i];
+		return *this;
+	}
+
+	template <unsigned int E, typename T>
+	vector<E, T>& vector<E, T>::operator *= (const T& rhs) {
+		for (auto i = 0u; i != E; ++i)
+			elements_[i] *= rhs;
+		return *this;
+	}
+
+	template <unsigned int E, typename T>
+	vector<E, T>& vector<E, T>::operator /= (const T& rhs) {
+		for (auto i = 0u; i != E; ++i)
+			elements_[i] /= rhs;
+		return *this;
+	}
+
+	template <unsigned int E, typename T>
+	auto vector<E, T>::set(unsigned int i, const T& n) -> vector<E, T>&
 	{
-	}
-
-	template <typename T>
-	vector<3, T>& vector<3, T>::operator = (const vector<3, T>& rhs) {
-		x_ = rhs.x_;
-		y_ = rhs.y_;
-		z_ = rhs.z_;
+		elements_[i] = n;
 		return *this;
 	}
 
-	template <typename T>
-	vector<3, T>& vector<3, T>::operator += (const vector<3, T>& rhs) {
-		x_ += rhs.x_;
-		y_ += rhs.y_;
-		z_ += rhs.z_;
-		return *this;
+
+	namespace expr_tmpl {
+		template <unsigned int E, typename T>
+		struct cast<vector<E,T>> {
+			template <typename OPER>
+			vector<E,T> operator ()(const expr<vector<E,T>, OPER>& o) const {
+				vector<E,T> result;
+				for (auto i = 0U; i != E; ++i)
+					result.set(i, o[i]);
+				return result;
+			}
+		};
 	}
 
-	template <typename T>
-	vector<3, T>& vector<3, T>::operator -= (const vector<3, T>& rhs) {
-		x_ -= rhs.x_;
-		y_ -= rhs.y_;
-		z_ -= rhs.z_;
-		return *this;
-	}
-
-	template <typename T>
-	vector<3, T>& vector<3, T>::operator *= (const T& rhs) {
-		x_ *= rhs;
-		y_ *= rhs;
-		z_ *= rhs;
-		return *this;
-	}
-
-	template <typename T>
-	vector<3, T>& vector<3, T>::operator /= (const T& rhs) {
-		x_ /= rhs;
-		y_ /= rhs;
-		z_ /= rhs;
-		return *this;
-	}
-
-	template <typename T>
-	auto vector<3, T>::set(const T& x, const T& y, const T& z) -> vector<3, T>&
+	template <unsigned int E, typename T>
+	struct vector_mul_post
 	{
-		x_ = x;
-		y_ = y;
-		z_ = z;
-		return *this;
-	}
+		auto operator ()(const vector<E,T>& lhs, const T& rhs, int i) const
+		 -> decltype(lhs[i]*rhs)
+		  { return lhs[i] * rhs; }
+	};
 
-	template <typename T>
-	vector<3, T>& vector<3, T>::set_x(const T& x)
+	template <unsigned int E, typename T>
+	struct vector_mul_pre
 	{
-		x_ = x;
-		return *this;
-	}
-
-	template <typename T>
-	vector<3, T>& vector<3, T>::set_y(const T& y)
-	{
-		y_ = y;
-		return *this;
-	}
-
-	template <typename T>
-	vector<3, T>& vector<3, T>::set_z(const T& z)
-	{
-		z_ = z;
-		return *this;
-	}
-
+		auto operator ()(const T& lhs, const vector<E,T>& rhs, int i) const
+		 -> decltype(lhs*rhs[i])
+		  { return lhs * rhs[i]; }
+	};
 
 	typedef vector<3, float> vector3f;
 
-	auto operator + (const vector3f& lhs, const vector3f& rhs)
-	 -> expr_tmpl::expr<vector3f, expr_tmpl::binary_oper<expr_tmpl::elementwise_add, vector3f, vector3f>>
-	  { return expr_tmpl::expr<vector3f, expr_tmpl::binary_oper<expr_tmpl::elementwise_add, vector3f, vector3f>>(lhs, rhs); }
+	template <unsigned int E, typename T>
+	auto dot_product(const vector<E,T>& lhs, const vector<E,T>& rhs) -> T
+	{
+		T result{};
+		for (auto i = 0u; i != E; ++i)
+			result += lhs[i] * rhs[i];
+		return result;
+	}
 
 
+	template <unsigned int E, typename T>
+	auto operator + (const vector<E,T>& lhs, const vector<E,T>& rhs)
+	 -> expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<expr_tmpl::elementwise_add, vector<E,T>, vector<E,T>>>
+	  { return expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<expr_tmpl::elementwise_add, vector<E,T>, vector<E,T>>>(lhs, rhs); }
 
-//	ATMA_MATH_OPERATOR_TX_TX(operator +, elementwise_add_oper, vector3f, vector3f, vector3f)
-//	ATMA_MATH_OPERATOR_TX_TX(operator -, elementwise_sub_oper, vector3f, vector3f, vector3f)
-	//ATMA_MATH_OPERATOR_T_TX(operator *, elementwise_mul_oper, float, vector3f, vector3f)
-	//ATMA_MATH_OPERATOR_TX_T(operator *, elementwise_mul_oper, vector3f, float, vector3f)
-	//ATMA_MATH_OPERATOR_TX_T(operator /, elementwise_div_oper, vector3f, float, vector3f)
+	template <unsigned int E, typename T, typename OPER>
+	auto operator + (const vector<E,T>& lhs, const expr_tmpl::expr<vector<E,T>, OPER>& rhs)
+	 -> expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<expr_tmpl::elementwise_add, vector<E,T>, decltype(rhs)>>
+	  { return expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<expr_tmpl::elementwise_add, vector<E,T>, decltype(rhs)>>(lhs, rhs); }
 	
-//	ATMA_MATH_OPERATOR_TX_TX(dot_product, vector_dotproduct_oper, vector3f, vector3f, vector3f)
+
+	template <unsigned int E, typename T, typename OPER>
+	auto operator * (const vector<E,T>& lhs, const expr_tmpl::expr<T,OPER>& rhs) ->
+	expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<vector_mul_post<E,T>, vector<E,T>, decltype(rhs)>>
+	{
+		return expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<vector_mul_post<E,T>, vector<E,T>, decltype(rhs)>>(lhs, rhs);
+	}
+
+	template <unsigned int E, typename T>
+	auto operator * (const vector<E,T>& lhs, const T& rhs) ->
+	expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<vector_mul_post<E,T>, vector<E,T>, T>>
+	{
+		return expr_tmpl::expr<vector<E,T>, expr_tmpl::binary_oper<vector_mul_post<E,T>, vector<E,T>, T>>(lhs, rhs);
+	}
+
 
 //=====================================================================
 } // namespace atma
