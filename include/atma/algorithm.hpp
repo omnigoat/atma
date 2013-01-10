eb6dd5b8c2ac39f6795e25afadec47bdb867c9ff
jona <omnigoat@gmail.com>
2013-01-10 12:34:49 +1100

    extra-special merging function, with bells AND whistles

--- include/atma/algorithm.hpp
+++ include/atma/algorithm.hpp
@@ -4,27 +4,49 @@
 namespace atma {
 //=====================================================================
 	
-	template <typename T>
-	struct default_predicate {
-		bool operator (const T& t) const {
-			return static_cast<bool>(t):
-		}
-	};
-	
-	
-	template <typename IT, typename OT, typename FN, typename PR = default_predicate<typename OT::value_type> >
-	OT transform_if(IT begin, IT end, OT out, FN op, PR pred)
+	//=====================================================================
+	// merge
+	// -------
+	//   takes two ranges, a less-than predicate, merging function, and two functions
+	//   for when merging fails, and merges equivalent elements together (using the
+	//   merging function), and outputs them into an output iterator. elements not
+	//   merged are passed to their respective merging function (one for each range)
+	//
+	//   returns the output iterator
+	//=====================================================================
+	template <typename LIT, typename RIT, typename OT, typename MERGER, typename LFN, typename RFN, typename PR>
+	OT merge(LIT xs_begin, LIT xs_end, RIT ys_begin, RIT ys_end, OT out, MERGER merger, LFN lfn, RFN rfn, PR pred)
 	{
-		while (begin != end) {
-			typename OT::value_type tmp = op(*begin);
-			if ( pred(tmp) )
-				*out = tmp;
-			
-			++begin;
-			++out;
+		static_assert(std::is_same<typename LIT::value_type, typename RIT::value_type>::value == true, "value-types different");
+
+		auto x = xs_begin;
+		auto y = ys_begin;
+		while (x != xs_end && y != ys_end) {
+			if (pred(*x, *y)) {
+				lfn(*x);
+				++x;
+			}
+			else if (pred(*y, *x)) {
+				rfn(*y);
+				++y;
+			}
+			else {
+				*out++ = merger(*x, *y);
+			}
 		}
-		
+
+		while (x != xs_end)
+			lfn(*x++);
+
+		while (y != ys_end)
+			rfn(*y++);
+
 		return out;
+	}
+
+	template <typename LIT, typename RIT, typename OT, typename MERGER, typename LFN, typename RFN>
+	OT merge(LIT xs_begin, LIT xs_end, RIT ys_begin, RIT ys_end, OT out, MERGER merger, LFN lfn, RFN rfn) {
+		return merge(xs_begin, xs_end, ys_begin, ys_end, out, merger, lfn, rfn, std::less<typename LIT::value_type>());
 	}
 
 
