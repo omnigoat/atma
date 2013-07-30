3ba17182a4825e4ac252f518182368bc28027b24
omnigoat <379771+omnigoat@users.noreply.github.com>
2013-07-30 14:39:25 +1000

    a little more work on utf8_string_range_t

--- include/atma/utf/utf8_string_range.hpp
+++ include/atma/utf/utf8_string_range.hpp
@@ -7,7 +7,18 @@
 namespace atma {
 //=====================================================================
 
-	// an immutable range
+	
+	//=====================================================================
+	// utf8_string_range_t
+	// ---------------------
+	//   A string-range that does not allocate any memory, but instead points
+	//   to external, contiguous memory that is expected to be valid for the
+	//   lifetime of the string-range.
+	//
+	//   This range is immutable, in that whilst we can change where we point
+	//   to, we can not change the contents of what we point to.
+	//
+	//=====================================================================
 	class utf8_string_range_t
 	{
 	public:
@@ -28,12 +39,33 @@ namespace atma {
 		char const* end_;
 	};
 
+	inline auto operator == (utf8_string_range_t const& lhs, utf8_string_range_t const& rhs) -> bool
+	{
+		return lhs.bytes() == rhs.bytes() && memcmp(lhs.begin(), rhs.begin(), lhs.bytes()) == 0;
+	}
+
+	inline auto operator != (utf8_string_range_t const& lhs, utf8_string_range_t const& rhs) -> bool
+	{
+		return !operator == (lhs, rhs);
+	}
+
+	inline auto operator < (utf8_string_range_t const& lhs, utf8_string_range_t const& rhs) -> bool
+	{
+		return
+		  lhs.bytes() < rhs.bytes() || !(rhs.bytes() < lhs.bytes()) &&
+		  memcmp(lhs.begin(), rhs.begin(), lhs.bytes()) < 0
+		  ;
+	}
+
 	inline auto operator << (std::ostream& stream, utf8_string_range_t const& xs) -> std::ostream&
 	{
 		for (auto x : xs)
 			stream.put(x);
 		return stream;
 	}
+
+
+
 
 	//=====================================================================
 	// utf8_string_range_t implementation
