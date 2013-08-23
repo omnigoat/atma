4ff9489032676f5ff77766c43c204995394e2a62
omnigoat <379771+omnigoat@users.noreply.github.com>
2013-08-23 17:52:42 +1000

    consolidate into just vector_add

--- include/atma/math/impl/expr.hpp
+++ include/atma/math/impl/expr.hpp
@@ -9,38 +9,20 @@ namespace math {
 namespace impl {
 //=====================================================================
 	
-	struct vector4f;
-
-	template <typename T>
-	struct base_expr {};
-
-#ifdef ATMA_MATH_USE_SSE
-	template <>
-	struct base_expr<vector4f>
-	{
-		__m128 xmmd;
-	};
-#endif
-
-
 	template <typename R, typename OPER>
 	struct expr
 	{
-		template <typename... Args>
-		expr(const Args&... args)
-		 : oper_(args...)
-		  {}
-
-#ifndef ATMA_MATH_USE_SSE
+#ifdef ATMA_MATH_USE_SSE
+		//auto xmmd() const -> decltype(std::declval<OPER>().xmmd()) {
+			//return static_cast<OPER const*>(this)->xmmd();
+		//}
+#else
 		auto operator [](unsigned int i) const
 		-> decltype(std::declval<OPER>()[i])
 		{
-			return oper_[i];
+			return static_cast<OPER const*>(this)->operator[](i);
 		}
 #endif
-
-	private:
-		OPER oper_;
 	};
 
 //=====================================================================
