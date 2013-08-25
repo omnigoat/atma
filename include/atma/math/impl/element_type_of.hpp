5dd69f4b635fbc10da89e82914009376b1ba3425
omnigoat <379771+omnigoat@users.noreply.github.com>
2013-08-25 21:35:45 +1000

    repurposed some files to solve the problem of declaration/definition order, and determining the element-type of dervied types for expr

--- include/atma/math/impl/element_type_of.hpp
+++ include/atma/math/impl/element_type_of.hpp
@@ -1,34 +1,21 @@
 //=====================================================================
 //
 //=====================================================================
-#ifndef ATMA_MATH_impl_BINARY_OPERATOR_HPP
-#define ATMA_MATH_impl_BINARY_OPERATOR_HPP
+#ifndef ATMA_MATH_IMPL_vector_FNS_HPP
+#define ATMA_MATH_IMPL_vector_FNS_HPP
 //=====================================================================
-#include <atma/math/impl/storage_policy.hpp>
 //=====================================================================
 namespace atma {
 namespace math {
 namespace impl {
 //=====================================================================
 
-	template <typename FN, typename LHS, typename RHS>
-	struct binary_oper
+	template <typename T>
+	struct element_type_of
 	{
-		binary_oper(const LHS& lhs, const RHS& rhs)
-		 : lhs(lhs), rhs(rhs)
-		  {}
-		
-		auto operator [](int i) const
-		 -> decltype(std::declval<FN>()(std::declval<LHS>(), std::declval<RHS>(), i))
-		  { return fn_(lhs, rhs, i); }
-	
-		typename storage_policy<LHS>::type lhs;
-		typename storage_policy<RHS>::type rhs;
-
-	private:
-		FN fn_;
+		typedef float type;
 	};
-		
+
 //=====================================================================
 } // namespace impl
 } // namespace math
---
+++ include/atma/math/impl/vector4f_exprs.hpp
@@ -0,0 +1,73 @@
+//=====================================================================
+//
+//=====================================================================
+#ifndef ATMA_MATH_IMPL_VECTOR4F_EXPRS_HPP
+#define ATMA_MATH_IMPL_VECTOR4F_EXPRS_HPP
+//=====================================================================
+#include <atma/math/impl/binary_expr.hpp>
+
+//=====================================================================
+namespace atma {
+namespace math {
+namespace impl {
+//=====================================================================
+
+	// vector4f_add
+	template <typename LHS, typename RHS>
+	struct vector4f_add : binary_expr<vector4f, vector4f_add, LHS, RHS>
+	{
+		vector4f_add(LHS const& lhs, RHS const& rhs) : binary_expr(lhs, rhs) {}
+
+#ifdef ATMA_MATH_USE_SSE
+		auto xmmd() const -> __m128 { return _mm_add_ps(xmmd_of(lhs), xmmd_of(rhs)); }
+#else
+		auto element(uint32_t i) const -> float { return element_of(lhs, i) + element_of(rhs, i); }
+#endif
+	};
+
+	// vector4f_sub
+	template <typename LHS, typename RHS>
+	struct vector4f_sub : binary_expr<vector4f, vector4f_sub, LHS, RHS>
+	{
+		vector4f_sub(LHS const& lhs, RHS const& rhs) : binary_expr(lhs, rhs) {}
+
+#ifdef ATMA_MATH_USE_SSE
+		auto xmmd() const -> __m128 { return _mm_sub_ps(xmmd_of(lhs), xmmd_of(rhs)); }
+#else
+		auto element(uint32_t i) const -> float { return element_of(lhs, i) - element_of(rhs, i); }
+#endif
+	};
+
+	// vector4f_mul
+	template <typename LHS, typename RHS>
+	struct vector4f_mul : binary_expr<vector4f, vector4f_mul, LHS, RHS>
+	{
+		vector4f_mul(LHS const& lhs, RHS const& rhs) : binary_expr(lhs, rhs) {}
+
+#ifdef ATMA_MATH_USE_SSE
+		auto xmmd() const -> __m128 { return _mm_mul_ps(xmmd_of(lhs), xmmd_of(rhs)); }
+#else
+		auto element(uint32_t i) const -> float { return element_of(lhs, i) * element_of(rhs, i); }
+#endif
+	};
+
+	// vector4f_div
+	template <typename LHS, typename RHS>
+	struct vector4f_div : binary_expr<vector4f, vector4f_div, LHS, RHS>
+	{
+		vector4f_div(LHS const& lhs, RHS const& rhs) : binary_expr(lhs, rhs) {}
+
+#ifdef ATMA_MATH_USE_SSE
+		auto xmmd() const -> __m128 { return _mm_div_ps(xmmd_of(lhs), xmmd_of(rhs)); }
+#else
+		auto element(uint32_t i) const -> float { return element_of(lhs, i) / element_of(rhs, i); }
+#endif
+	};
+
+//=====================================================================
+} // namespace impl
+} // namespace math
+} // namespace atma
+//=====================================================================
+#endif
+//=====================================================================
