9f17e1c672a22209a3e72c336d90e8016fe4628a
omnigoat <379771+omnigoat@users.noreply.github.com>
2013-09-05 12:12:41 +1000

    testing out some element reference stuff for matrices. doesn't seem to affect release code generation at all

--- include/atma/math/impl/matrix4f_declaration.hpp
+++ include/atma/math/impl/matrix4f_declaration.hpp
@@ -26,16 +26,97 @@ namespace math {
 		explicit matrix4f(__m128 const& r0, __m128 const& r1, __m128 const& r2, __m128 const& r3);
 #endif
 
+		template <typename T>
+		struct cell_element_ref
+		{
+			cell_element_ref(matrix4f* owner, uint32_t row, uint32_t col)
+			: owner_(owner), row_(row), col_(col)
+			{}
+
+			auto operator = (float rhs) -> float
+			{
+				return owner_->rmd_[row_].m128_f32[col_] = rhs;
+			}
+
+			operator float() {
+				return owner_->xmmd(row_).m128_f32[col_];
+			}
+
+		private:
+			matrix4f* owner_;
+			uint32_t row_, col_;
+		};
+
+		template <typename T>
+		struct cell_element_ref<T const>
+		{
+			cell_element_ref(matrix4f const* owner, uint32_t row, uint32_t col)
+			: owner_(owner), row_(row), col_(col)
+			{}
+
+			operator float() {
+				return owner_->xmmd(row_).m128_f32[col_];
+			}
+
+		private:
+			matrix4f const* owner_;
+			uint32_t row_, col_;
+		};
+
+
+		template <typename T>
+		struct row_element_ref
+		{
+			row_element_ref(matrix4f* owner, uint32_t row)
+			: owner_(owner), row_(row)
+			{}
+
+			auto operator[](int i) -> cell_element_ref<T>
+			{
+				return cell_element_ref<T>(owner_, row_, i);
+			}
+
+			auto operator[](uint32_t i) const -> cell_element_ref<T const>
+			{
+				return cell_element_ref<T const>(owner_, row_, i);
+			}
+
+		private:
+			matrix4f* owner_;
+			uint32_t row_;
+		};
+
+		template <typename T>
+		struct row_element_ref<T const>
+		{
+			row_element_ref(matrix4f const* owner, uint32_t row)
+			: owner_(owner), row_(row)
+			{}
+
+			auto operator[](uint32_t i) const -> cell_element_ref<T const>
+			{
+				return cell_element_ref<T const>(owner_, row_, i);
+			}
+
+		private:
+			matrix4f const* owner_;
+			uint32_t row_;
+		};
+
 		// operators
 		auto operator = (matrix4f const&) -> matrix4f&;
-
+		auto operator[](uint32_t i) -> row_element_ref<float> { return {this, i}; }
+		auto operator[](uint32_t i) const -> row_element_ref<float const> { return {this, i}; }
 
 		// computation
 		auto transposed() const -> matrix4f;
 		auto inverted() const -> matrix4f;
-
-		auto set(uint32_t r, uint32_t c, float v) -> void;
 		
+		// mutators
+		auto set(uint32_t r, uint32_t c, float v) -> void;
+		auto transpose() -> void;
+		auto invert() -> void;
+	
 
 #ifdef ATMA_MATH_USE_SSE
 		auto xmmd(uint32_t i) const -> __m128 const&;
