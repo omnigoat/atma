f93d1b7d7e58603614304c39bbd8d2de119de5a4
omnigoat <379771+omnigoat@users.noreply.github.com>
2013-10-25 02:14:29 +1100

    added platform.hpp back to project, added enable_multiple_shared_from_this.hpp, a concept I'm not entirely sold on

---
+++ include/atma/enable_multiple_shared_from_this.hpp
@@ -0,0 +1,29 @@
+#ifndef ATMA_ENABLE_MUIPTLE_SHARED_FROM_THIS_HPP
+#define ATMA_ENABLE_MUIPTLE_SHARED_FROM_THIS_HPP
+//=====================================================================
+#include <memory>
+//=====================================================================
+namespace atma {
+//=====================================================================
+	
+	struct enable_multiple_shared_from_this : std::enable_shared_from_this<enable_multiple_shared_from_this>
+	{
+		virtual ~enable_multiple_shared_from_this() {}
+
+		template <typename T>
+		auto shared_from_this() -> std::shared_ptr<T> { 
+			return std::dynamic_pointer_cast<T>(::std::enable_shared_from_this<enable_multiple_shared_from_this>::shared_from_this());
+		}
+
+		template <typename T>
+		auto shared_from_this() const -> std::shared_ptr<T const> {
+			return std::dynamic_pointer_cast<T const>(::std::enable_shared_from_this<enable_multiple_shared_from_this>::shared_from_this());
+		}
+	};
+
+//=====================================================================
+} // namespace atma
+//=====================================================================
+#endif // inclusion guard
+//=====================================================================
+
