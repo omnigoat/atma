72dfedc6037fcdb50aac33398493893686d2e9a9
omnigoat <379771+omnigoat@users.noreply.github.com>
2014-01-03 22:35:44 +1100

    renamed signal_hq to thread/engine, removed shutdown-queue as it could easily cause order-of-destruction problems as a class-member

---
+++ include/atma/thread/engine.hpp
@@ -0,0 +1,99 @@
+#ifndef ATMA_THREAD_ENGINE_HP
+#define ATMA_THREAD_ENGINE_HP
+//=====================================================================
+#include <atma/lockfree/queue.hpp>
+#include <vector>
+#include <thread>
+#include <algorithm>
+//=====================================================================
+namespace atma {
+namespace thread {
+//=====================================================================
+	
+	struct engine_t
+	{
+		typedef std::function<void()> signal_t;
+
+		engine_t();
+		~engine_t();
+
+		auto signal(signal_t const&) -> void;
+		auto signal_block() -> void;
+		
+	private:
+		auto reenter(std::atomic_bool const& blocked) -> void;
+
+		std::thread handle_;
+		bool running_;
+		atma::lockfree::queue_t<signal_t> queue_;
+	};
+
+
+
+	inline engine_t::engine_t()
+		: running_(true)
+	{
+		handle_ = std::thread([&]
+		{
+			// continue performing all commands always and forever
+			while (running_) {
+				signal_t x;
+				while (queue_.pop(x)) {
+					x();
+				}
+			}
+		});
+	}
+
+	inline engine_t::~engine_t()
+	{
+		signal([&] { running_ = false; });
+		handle_.join();
+	}
+
+
+	inline auto engine_t::reenter(std::atomic_bool const& blocked) -> void
+	{
+		// continue performing all commands always and forever
+		signal_t x;
+		while (blocked && queue_.pop(x)) {
+			x();
+		}
+	}
+
+	inline auto engine_t::signal(signal_t const& fn) -> void
+	{
+		if (!running_)
+			return;
+		queue_.push(fn);
+	}
+
+	inline auto engine_t::signal_block() -> void
+	{
+		if (!running_)
+			return;
+
+		// push blocking fn!
+		auto blocked = std::atomic_bool{true};
+		queue_.push([&blocked]{ blocked = false; });
+
+		// don't block if we're the prime thread blocking ourselves.
+		if (std::this_thread::get_id() == handle_.get_id())
+		{
+			reenter(blocked);
+			return;
+		}
+		else
+		{
+			while (blocked)
+				;
+		}
+	}
+
+//=====================================================================
+} // namespace thread
+} // namespace atma
+//=====================================================================
+#endif // inclusion guard
+//=====================================================================
+
