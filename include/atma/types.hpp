1ea1020d5caa3d03831b4e107b84ce5a341902e5
omnigoat <379771+omnigoat@users.noreply.github.com>
2014-02-09 16:21:58 +1100

    ATMA_ENSURE has a local variable for inspection on break, added types.hpp, because 64bit

---
+++ include/atma/types.hpp
@@ -0,0 +1,26 @@
+#pragma once
+//======================================================================
+#include <cstdint>
+//======================================================================
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+typedef long llong;
+typedef unsigned long long ullong;
+
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef intptr_t intptr;
+typedef uintptr_t uintptr;
+
