d6df8cce83380f86f00a1fb831d66a0871030b18
jona <omnigoat@gmail.com>
2014-09-23 12:46:41 +1000

    updates to string

--- include/atma/utf/utf8_string_header.hpp
+++ include/atma/utf/utf8_string_header.hpp
@@ -1,5 +1,5 @@
 #pragma once
-//=====================================================================
+
 #include <atma/types.hpp>
 #include <atma/assert.hpp>
 #include <atma/utf/algorithm.hpp>
@@ -7,17 +7,13 @@
 #include <string>
 #include <vector>
 #include <cstdint>
-//=====================================================================
-namespace atma {
-//=====================================================================
 
-	//=====================================================================
-	// we use these
-	//=====================================================================
+
+namespace atma {
+
 	struct utf8_char_t;
 	class utf8_string_t;
 	class utf8_string_range_t;
-	
 
 
 	struct utf8_char_t
@@ -62,31 +58,33 @@ namespace atma {
 		typedef iterator_t<utf8_char_t const> const_iterator;
 
 		utf8_string_t();
+		utf8_string_t(const_iterator const&, const_iterator const&);
 		utf8_string_t(char const* str);
 		utf8_string_t(char const* str_begin, char const* str_end);
 		utf8_string_t(utf8_string_range_t const&);
-		//explicit utf8_string_t(utf16_string_t const&);
 		utf8_string_t(utf8_string_t const&);
 		utf8_string_t(utf8_string_t&&);
 
-		
-
 		auto empty() const -> bool;
 		auto bytes() const -> size_t;
+		auto c_str() const -> char const*;
 
 		auto begin() const -> const_iterator;
 		auto end() const -> const_iterator;
 		auto begin() -> iterator;
 		auto end() -> iterator;
 
-		auto bytes_begin() const -> char const*;
-		auto bytes_end() const -> char const*;
-		auto bytes_begin() -> char*;
-		auto bytes_end() -> char*;
+		auto begin_raw() const -> char const*;
+		auto end_raw() const -> char const*;
+		auto begin_raw() -> char*;
+		auto end_raw() -> char*;
 
-		// push back a single character is valid only for code-points < 128
+
 		auto push_back(char c) -> void;
 		auto push_back(utf8_char_t const& c) -> void;
+
+		//template <typename IT> auto insert(const_iterator const&, IT const&, IT const&);
+		//auto insert(const_iterator const&, char const*, char const*) -> void;
 
 		auto clear() -> void;
 
@@ -105,18 +103,26 @@ namespace atma {
 		friend auto operator << (std::ostream& out, utf8_string_t const& rhs) -> std::ostream&;
 	};
 
+	auto operator == (utf8_string_t const&, utf8_string_t const&) -> bool;
+	auto operator != (utf8_string_t const&, utf8_string_t const&) -> bool;
+
+	auto operator == (utf8_string_t const&, char const*) -> bool;
+	auto operator == (char const*, utf8_string_t const&) -> bool;
 
 
 
 	template <typename T>
-	class utf8_string_t::iterator_t : std::iterator<std::bidirectional_iterator_tag, T>
+	class utf8_string_t::iterator_t : public std::iterator<std::bidirectional_iterator_tag, T>
 	{
+		friend class utf8_string_t;
+
+		using source_container_t = std::conditional_t<std::is_const<T>::value, utf8_string_t const, utf8_string_t>;
+		using source_iterator  = std::conditional_t<std::is_const<T>::value, utf8_string_t::chars_t::const_iterator, utf8_string_t::chars_t::iterator>;
+		using source_char_t = std::conditional_t<std::is_const<T>::value, char const, char>;
+
 	public:
-		typedef typename std::conditional<std::is_const<T>::value, utf8_string_t const, utf8_string_t>::type container_t;
-		typedef typename std::conditional<std::is_const<T>::value, char const*, char*>::type char_ptr;
-		
-		iterator_t(container_t& owner, decltype(std::declval<T>().begin) x)
-		: owner_(&owner), char_(x, utf8_next_char(x))
+		iterator_t(source_container_t& owner, source_char_t* x)
+			: owner_(&owner), char_(x, utf8_next_char(x))
 		{}
 
 		iterator_t(iterator_t const& rhs)
@@ -139,11 +145,19 @@ namespace atma {
 			return char_;
 		}
 
-		auto operator ++ () -> iterator_t& {
-			ATMA_ASSERT(char_.begin != owner_->bytes_end());
+		auto operator ++ () -> iterator_t&
+		{
+			ATMA_ASSERT(char_.begin != owner_->end_raw());
+			auto mv = 
 			char_.begin = char_.end;
 			char_.end = utf8_next_char(char_.end);
 			return *this;
+		}
+
+		auto operator ++ (int) -> iterator_t {
+			auto t = *this;
+			++*this;
+			return t;
 		}
 
 		auto operator -> () const -> T* {
@@ -151,7 +165,7 @@ namespace atma {
 		}
 
 	private:
-		container_t* owner_;
+		source_container_t* owner_;
 		utf8_char_t char_;
 
 		template <typename>
