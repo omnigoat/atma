a7f8f20518ad6b565107262cfc1dabb22865bb72
omnigoat <379771+omnigoat@users.noreply.github.com>
2014-11-27 18:19:28 +1100

    updates to maths

--- include/atma/math/vector4f.hpp
+++ include/atma/math/vector4f.hpp
@@ -1,11 +1,7 @@
-//=====================================================================
-//
-//=====================================================================
-#ifndef ATMA_MATH_VECTOR4F_HPP
-#define ATMA_MATH_VECTOR4F_HPP
-//=====================================================================
+#pragma once
+
 #define ATMA_MATH_VECTOR4F_SCOPE
-//=====================================================================
+
 #include <cstdint>
 #include <atma/types.hpp>
 #include <atma/math/impl/vector4f_fwd.hpp>
@@ -14,8 +10,5 @@
 #include <atma/math/impl/vector4f_exprs.hpp>
 #include <atma/math/impl/vector4f_definition.hpp>
 #include <atma/math/impl/vector4f_operators.hpp>
-//=====================================================================
+
 #undef ATMA_MATH_VECTOR4F_SCOPE
-//=====================================================================
-#endif
-//=====================================================================
