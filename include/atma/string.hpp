642b6c84924884bc7d9236f8a946a48bc516ab53
omnigoat <379771+omnigoat@users.noreply.github.com>
2014-12-16 12:07:16 +1100

    rewrote to_string for correctness. formatting.

--- include/atma/string.hpp
+++ include/atma/string.hpp
@@ -1,41 +1,36 @@
-#ifndef ATMA_STRING
-#define ATMA_STRING
-//=====================================================================
-#include <vector>
-#include <iterator>
-#include <algorithm>
-//=====================================================================
-#include <atma/assert.hpp>
-#include <atma/utf/algorithm.hpp>
+#pragma once
+
 #include <atma/utf/utf8_string.hpp>
-#include <atma/utf/utf8_string_range.hpp>
-//=====================================================================
+#include <atma/enable_if.hpp>
+
+#include <algorithm>
+
+
 namespace atma {
-//=====================================================================
-	
 
 	// bam.
 	typedef utf8_string_t string;
 
-
-	inline auto to_string(uint32 n) -> atma::string
+	template <typename T, typename = atma::enable_if<std::is_integral<T>, std::is_unsigned<T>>>
+	inline auto to_string(T x, uint base = 10) -> atma::string
 	{
 		atma::string s;
-		while (n) {
-			s.push_back(n % 10 + 0x30);
-			n /= 10;
-		}
+
+		// do characters
+		do {
+			auto xb = x % base;
+
+			if (xb > 9)
+				s.push_back(xb - 10 + 'a');
+			else
+				s.push_back(xb + '0');
+
+			x /= base;
+		} while (x > 0);
 
 		std::reverse(s.raw_begin(), s.raw_end());
 
 		return s;
 	}
 
-
-
-//=====================================================================
-} // namespace atma
-//=====================================================================
-#endif // inclusion guard
-//=====================================================================
-
+}
