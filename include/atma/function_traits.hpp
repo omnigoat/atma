ff7f6dc2ab76c731737037558b36b27b745e4bcf
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-02-04 14:20:42 +1100

    updated function_traits to be nicer

--- include/atma/function_traits.hpp
+++ include/atma/function_traits.hpp
@@ -5,68 +5,62 @@
 
 namespace atma {
 
+	template <typename F>
+	struct function_traits;
+
+
+
+
+	// remove references
+	template <typename F>
+	struct function_traits<F&>
+		: function_traits<F>
+	{};
+
+	template <typename F>
+	struct function_traits<F&&>
+		: function_traits<F>
+	{};
+
+
+	// presumed "callable"
 	template <typename T>
 	struct function_traits
 		: function_traits<decltype(&T::operator())>
 	{};
 
-	template <typename T>
-	struct function_traits<T&>
-		: function_traits<T>
+
+	// function-pointer
+	template <typename R, typename... Args>
+	struct function_traits<R(*)(Args...)>
+		: function_traits<R(Args...)>
 	{};
 
-	template <typename T>
-	struct function_traits<T&&>
-		: function_traits<T>
+
+	// member-function-pointer
+	template <typename C, typename R, typename... Args>
+	struct function_traits<R(C::*)(Args...) const>
+		: function_traits<R(Args...)>
 	{};
 
-	template <typename T>
-	struct function_traits<T*>
-		: function_traits<T>
+	template <typename C, typename R, typename... Args>
+	struct function_traits<R(C::*)(Args...)>
+		: function_traits<R(Args...)>
 	{};
+
+
+
 
 	template <typename R, typename... Args>
-	struct function_traits<R(&)(Args...)>
-		: function_traits<R(*)(Args...)>
-	{};
-
-
-
-
-	template <typename C, typename R, typename... Params>
-	struct function_traits<R(C::*)(Params...) const>
+	struct function_traits<R(Args...)>
 	{
 		using result_type      = R;
-		using tupled_args_type = std::tuple<Params...>;
-		
-		template <size_t i>
-		using arg_type = typename std::tuple_element<i, std::tuple<Params...>>::type;
-
-		static size_t const arity = sizeof...(Params);
-	};
-
-	template <typename C, typename R, typename... Params>
-	struct function_traits<R(C::*)(Params...)>
-	{
-		using result_type      = R;
-		using tupled_args_type = std::tuple<Params...>;
+		using tupled_args_type = std::tuple<Args...>;
 
 		template <size_t i>
-		using arg_type = typename std::tuple_element<i, std::tuple<Params...>>::type;
+		using arg_type = typename std::tuple_element<i, std::tuple<Args...>>::type;
 
-		static size_t const arity = sizeof...(Params);
-	};
-
-	template <typename R, typename... Params>
-	struct function_traits<R(*)(Params...)>
-	{
-		using result_type      = R;
-		using tupled_args_type = std::tuple<Params...>;
-
-		template <size_t i>
-		using arg_type = typename std::tuple_element<i, std::tuple<Params...>>::type;
-
-		static size_t const arity = sizeof...(Params);
+		static size_t const arity = sizeof...(Args);
 	};
 
 }
