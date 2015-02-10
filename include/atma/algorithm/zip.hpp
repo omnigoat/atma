f4e88b92ab2183983455b0b0dabefda2081011fd
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-02-10 12:30:26 +1100

    auto_tuple saving the day from the xvalue plague
     - zip is getting refactored to be nicer and correct
     - map getting a slight facelift (removing either_iterator)
     - auto_tuple/tuple stuff in general getting fixed

--- include/atma/algorithm/zip.hpp
+++ include/atma/algorithm/zip.hpp
@@ -15,17 +15,53 @@ namespace atma
 		};
 	}
 
+	template <typename...> struct zip_range_t;
+	template <typename...> struct zip_range_iterator_t;
+
+
+	template <typename... Ranges>
+	struct zip_range_t
+	{
+		using value_type      = std::tuple<typename std::remove_reference<Ranges>::type::value_type...>;
+		using reference       = std::tuple<typename std::remove_reference<Ranges>::type::reference...>;
+		using const_reference = std::tuple<typename std::remove_reference<Ranges>::type::const_reference...>;
+		using iterator        = zip_range_iterator_t<Ranges...>;
+		using const_iterator  = zip_range_iterator_t<Ranges const...>;
+
+		// seriously: what.
+		using difference_type = ptrdiff_t;
+		using size_type       = size_t;
+
+
+		zip_range_t(Ranges... ranges);
+
+		auto begin() -> iterator;
+		auto end() -> iterator;
+		auto begin() const -> const_iterator;
+		auto end() const -> const_iterator;
+
+	private:
+		std::tuple<Ranges...> ranges_;
+	};
+
+
 
 	template <typename... Ranges>
 	struct zip_range_iterator_t
 	{
 		using range_tuple_type = std::tuple<Ranges...>;
-		using value_type = std::tuple<typename std::remove_reference<Ranges>::type::value_type...>;
-		using reference = std::tuple<typename std::remove_reference<Ranges>::type::reference...>;
 
-		using internal_iter_tuple = std::tuple<typename detail::zip_internal_range_iterator_t<Ranges>::type...>;
+		using iterator_category = std::forward_iterator_tag;
+		using value_type        = std::tuple<typename std::remove_reference<Ranges>::type::value_type...>;
+		using difference_type   = ptrdiff_t;
+		using distance_type     = ptrdiff_t;
+		using pointer           = value_type*;
+		using reference         = std::tuple<typename std::remove_reference<Ranges>::type::reference...>;
 
-		zip_range_iterator_t(internal_iter_tuple const& iters)
+
+		using internal_iters_tuple_t = std::tuple<typename detail::zip_internal_range_iterator_t<Ranges>::type...>;
+
+		zip_range_iterator_t(internal_iters_tuple_t const& iters)
 			: iters_{iters}
 		{
 		}
@@ -42,13 +78,53 @@ namespace atma
 		}
 
 	private:
-		internal_iter_tuple iters_;
+		internal_iters_tuple_t iters_;
 
 		template <typename... Ranges>
 		friend auto operator == (zip_range_iterator_t<Ranges...> const& lhs, zip_range_iterator_t<Ranges...> const& rhs) -> bool;
 	};
 
 
+
+
+	//======================================================================
+	// RANGE IMPLEMENTATION
+	//======================================================================
+	template <typename... Ranges>
+	zip_range_t<Ranges...>::zip_range_t(Ranges... ranges)
+		: ranges_{ranges...}
+	{
+	}
+
+	template <typename... Ranges>
+	auto zip_range_t<Ranges...>::begin() -> iterator
+	{
+		return {atma::tuple_apply<atma::begin_adaptor_t>(ranges_)};
+	}
+
+	template <typename... Ranges>
+	auto zip_range_t<Ranges...>::end() -> iterator
+	{
+		return {atma::tuple_apply<atma::end_adaptor_t>(ranges_)};
+	}
+
+	template <typename... Ranges>
+	auto zip_range_t<Ranges...>::begin() const -> const_iterator
+	{
+		return {atma::tuple_apply<atma::begin_adaptor_t>(ranges_)};
+	}
+
+	template <typename... Ranges>
+	auto zip_range_t<Ranges...>::end() const -> const_iterator
+	{
+		return {atma::tuple_apply<atma::end_adaptor_t>(ranges_)};
+	}
+
+
+
+	//======================================================================
+	// ITERATOR IMPLEMENTATION
+	//======================================================================
 	template <typename... Ranges>
 	auto operator == (zip_range_iterator_t<Ranges...> const& lhs, zip_range_iterator_t<Ranges...> const& rhs) -> bool
 	{
@@ -82,28 +158,6 @@ namespace atma
 	}
 
 
-	template <typename... Ranges>
-	struct zip_range_t
-	{
-		zip_range_t(Ranges... ranges)
-			: ranges_{ranges...}
-		{
-		}
-
-		auto begin() -> zip_range_iterator_t<Ranges...>
-		{
-			return{atma::tuple_apply<atma::begin_adaptor_t>(ranges_)};
-		}
-
-		auto end() -> zip_range_iterator_t<Ranges...>
-		{
-			return{atma::tuple_apply<atma::end_adaptor_t>(ranges_)};
-		}
-
-	private:
-		std::tuple<Ranges...> ranges_;
-
-	};
 
 
 	template <typename... Ranges>
