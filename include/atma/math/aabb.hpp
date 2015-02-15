c57e5ec2bd78227b899f962ed080334ed30366d6
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-02-16 08:52:28 +1100

    aabb & unique_memory updates
     - unique_memory learns empty()
     - unique_memory learns swap()
     - aabb is learning...

--- include/atma/math/aabb.hpp
+++ include/atma/math/aabb.hpp
@@ -7,10 +7,26 @@
 
 namespace atma { namespace math {
 
+	struct from_cube_tag_t {};
+
+	namespace { from_cube_tag_t const from_cube_tag; }
+
+
 	struct aabb_t
 	{
+		aabb_t()
+			: origin_{math::point4f(0.f, 0.f, 0.f)}
+			, extents_{0.5f, 0.5f, 0.5f, 0.f}
+		{}
+
 		aabb_t(vector4f const& origin, vector4f const& extents)
-			: origin_(origin), extents_(extents)
+			: origin_{origin}
+			, extents_{extents}
+		{}
+
+		aabb_t(from_cube_tag_t, vector4f const& cube)
+			: origin_{math::point4f(cube.x, cube.y, cube.z)}
+			, extents_{cube.w, cube.w, cube.w, 0.f}
 		{}
 
 		static auto from_minmax(vector4f const& min, vector4f const& max) -> aabb_t
@@ -38,6 +54,16 @@ namespace atma { namespace math {
 			};
 		}
 
+		auto octant(int idx) -> aabb_t
+		{
+			return aabb_t{
+				math::point4f(
+					(-0.5f + float((idx & 1))) * extents_.x + origin_.x,
+					(-0.5f + float((idx & 2) >> 1)) * extents_.y + origin_.y,
+					(-0.5f + float((idx & 4) >> 2)) * extents_.z + origin_.z),
+				0.5f * extents_};
+		}
+
 		auto inside(math::vector4f const& p) const -> bool
 		{
 			return !(
@@ -46,7 +72,7 @@ namespace atma { namespace math {
 				p.z < origin_.z - extents_.z || origin_.z + extents_.z < p.z);
 		}
 
-		auto octant(math::vector4f const& p) const -> int
+		auto octant_idx(math::vector4f const& p) const -> int
 		{
 			return (int)(origin_.x < p.x) + 2*(int)(origin_.y < p.y) + 4*(int)(origin_.z < p.z);
 		}
--- include/atma/unique_memory.hpp
+++ include/atma/unique_memory.hpp
@@ -14,15 +14,20 @@ namespace atma
 		unique_memory_t(uint alignment, size_t size, void* data);
 		unique_memory_t(unique_memory_t&&);
 		~unique_memory_t();
-
 		unique_memory_t(unique_memory_t const&) = delete;
+
+		auto operator = (unique_memory_t&&) -> unique_memory_t&;
 		auto operator = (unique_memory_t const&) -> unique_memory_t& = delete;
 
+		auto empty() const -> bool;
 		auto size() const -> size_t;
-		auto begin() -> void*;
-		auto end() -> void*;
 		auto begin() const -> void const*;
 		auto end() const -> void const*;
+		operator bool() const;
+
+		auto begin() -> void*;
+		auto end() -> void*;
+		auto swap(unique_memory_t&) -> void;
 
 	private:
 		void* data_;
@@ -75,6 +80,21 @@ namespace atma
 		platform::deallocate_aligned_memory(data_);
 	}
 
+	inline auto unique_memory_t::operator = (unique_memory_t&& rhs) -> unique_memory_t&
+	{
+		this->~unique_memory_t();
+		data_ = rhs.data_;
+		end_ = rhs.end_;
+		rhs.data_ = nullptr;
+		rhs.end_ = nullptr;
+		return *this;
+	}
+
+	inline auto unique_memory_t::empty() const -> bool
+	{
+		return size() == 0;
+	}
+
 	inline auto unique_memory_t::size() const -> size_t
 	{
 		return reinterpret_cast<char const*>(end_) - reinterpret_cast<char const*>(data_);
@@ -98,6 +118,18 @@ namespace atma
 	inline auto unique_memory_t::end() const -> void const*
 	{
 		return end_;
+	}
+
+	inline auto unique_memory_t::swap(unique_memory_t& rhs) -> void
+	{
+		auto tmp = std::move(*this);
+		*this = std::move(rhs);
+		rhs = std::move(tmp);
+	}
+
+	inline unique_memory_t::operator bool() const
+	{
+		return data_ != nullptr;
 	}
 }
 
