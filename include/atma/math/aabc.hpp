e4cf423ed5d69fe23dc34e0342ef72c5c8681659
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-02-16 17:53:07 +1100

    unique_memory gets typing, updates to aabc
     - typed_unique_memory_t<T> now exists
     - unique_memory_t = typed_unique_memory_t<void>

--- include/atma/math/aabc.hpp
+++ include/atma/math/aabc.hpp
@@ -21,53 +21,64 @@ namespace atma { namespace math {
 			: data_(x, y, z, w)
 		{}
 
-		auto origin() const -> vector4f { return point4f(data_.x, data_.y, data_.z); }
-		auto radius() const -> float    { return data_.w; }
-		auto volume() const -> float    { return data_.w * data_.w * data_.w; }
+		auto origin() const       -> vector4f { return point4f(data_.x, data_.y, data_.z); }
+		auto radius() const       -> float    { return 0.5f * data_.w; }
+		auto volume() const       -> float    { return data_.w * data_.w * data_.w; }
+		auto surface_area() const -> float    { return 6 * data_.w * data_.w; }
 
-		auto compute_corners() const -> std::array<vector4f, 8>
-		{
-			auto origin = vector4f{data_.x, data_.y, data_.z, 1.f};
+		auto octant_idx_of(math::vector4f const&) const -> int;
+		auto octant_of(int) const -> aabc_t;
+		auto inside(math::vector4f const&) const -> bool;
 
-			return {
-				origin + 0.5f * vector4f(-data_.w, -data_.w, -data_.w, 0.f),
-				origin + 0.5f * vector4f( data_.w, -data_.w, -data_.w, 0.f),
-				origin + 0.5f * vector4f(-data_.w,  data_.w, -data_.w, 0.f),
-				origin + 0.5f * vector4f( data_.w,  data_.w, -data_.w, 0.f),
-				origin + 0.5f * vector4f(-data_.w, -data_.w,  data_.w, 0.f),
-				origin + 0.5f * vector4f( data_.w, -data_.w,  data_.w, 0.f),
-				origin + 0.5f * vector4f(-data_.w,  data_.w,  data_.w, 0.f),
-				origin + 0.5f * vector4f( data_.w,  data_.w,  data_.w, 0.f),
-			};
-		}
-
-		auto octant(int idx) -> aabc_t
-		{
-			auto subr = 0.5f * data_.w;
-
-			return aabc_t{
-				math::point4f(
-					(-0.5f + float((idx & 1) >> 0)) * subr + data_.x,
-					(-0.5f + float((idx & 2) >> 1)) * subr + data_.y,
-					(-0.5f + float((idx & 4) >> 2)) * subr + data_.z),
-					subr};
-		}
-
-		auto inside(math::vector4f const& p) const -> bool
-		{
-			return !(
-				p.x < data_.x - data_.w * 0.5f || data_.x + data_.w < p.x ||
-				p.y < data_.y - data_.w * 0.5f || data_.y + data_.w < p.y ||
-				p.z < data_.z - data_.w * 0.5f || data_.z + data_.w < p.z);
-		}
-
-		auto octant_idx(math::vector4f const& p) const -> int
-		{
-			return (int)(data_.x < p.x) + 2*(int)(data_.y < p.y) + 4*(int)(data_.z < p.z);
-		}
+		auto compute_corners() const -> std::array<vector4f, 8>;
 
 	private:
 		vector4f data_;
 	};
+
+
+
+
+	auto aabc_t::octant_idx_of(math::vector4f const& p) const -> int
+	{
+		return (int)(data_.x < p.x) + 2*(int)(data_.y < p.y) + 4*(int)(data_.z < p.z);
+	}
+
+	inline auto aabc_t::octant_of(int idx) const -> aabc_t
+	{
+		auto subr = 0.5f * data_.w;
+
+		return aabc_t {
+			math::point4f(
+			(-0.5f + float((idx & 1) >> 0)) * subr + data_.x,
+			(-0.5f + float((idx & 2) >> 1)) * subr + data_.y,
+			(-0.5f + float((idx & 4) >> 2)) * subr + data_.z),
+			subr};
+	}
+
+	inline auto aabc_t::inside(math::vector4f const& p) const -> bool
+	{
+		return !(
+			p.x < data_.x - data_.w * 0.5f || data_.x + data_.w < p.x ||
+			p.y < data_.y - data_.w * 0.5f || data_.y + data_.w < p.y ||
+			p.z < data_.z - data_.w * 0.5f || data_.z + data_.w < p.z);
+	}
+
+	auto aabc_t::compute_corners() const -> std::array<vector4f, 8>
+	{
+		auto origin = vector4f {data_.x, data_.y, data_.z, 1.f};
+
+		return {
+			origin + 0.5f * vector4f(-data_.w, -data_.w, -data_.w, 0.f),
+			origin + 0.5f * vector4f(data_.w, -data_.w, -data_.w, 0.f),
+			origin + 0.5f * vector4f(-data_.w, data_.w, -data_.w, 0.f),
+			origin + 0.5f * vector4f(data_.w, data_.w, -data_.w, 0.f),
+			origin + 0.5f * vector4f(-data_.w, -data_.w, data_.w, 0.f),
+			origin + 0.5f * vector4f(data_.w, -data_.w, data_.w, 0.f),
+			origin + 0.5f * vector4f(-data_.w, data_.w, data_.w, 0.f),
+			origin + 0.5f * vector4f(data_.w, data_.w, data_.w, 0.f),
+		};
+	}
+
 
 }}
