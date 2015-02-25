cf29547357d4c949d0c78e6e4f1bdfa293e18293
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-02-25 23:09:21 +1100

    vector4i gets love after all these years

--- include/atma/math/vector4i.hpp
+++ include/atma/math/vector4i.hpp
@@ -1,23 +1,12 @@
-//=====================================================================
-//
-//=====================================================================
-#ifndef ATMA_MATH_VECTOR4I_HPP
-#define ATMA_MATH_VECTOR4I_HPP
-//=====================================================================
-#define ATMA_MATH_VECTOR4I_SCOPE
-//=====================================================================
-#include <atma/math/impl/vector4i_declaration.hpp>
-//#include <atma/math/impl/vector4i_exprs.hpp>
-#include <atma/math/impl/vector4f_definition.hpp>
-//#include <atma/math/impl/vector4f_operators.hpp>
+#pragma once
 
-#include <cstdint>
+#define ATMA_MATH_VECTOR4I_SCOPE
+
+#include <atma/math/impl/vector4i_declaration.hpp>
+#include <atma/math/impl/vector4i_definition.hpp>
 
 #pragma warning(push,3)
 #include <emmintrin.h>
 #pragma warning(pop)
-//=====================================================================
+
 #undef ATMA_MATH_VECTOR4I_SCOPE
-//=====================================================================
-#endif
-//=====================================================================
