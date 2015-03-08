23e3daf1e1f58d09b89235aecc8053b5c0e0cb70
jona <omnigoat@gmail.com>
2015-03-08 20:05:09 +1100

    actual intersection! zx plane out of nowhere

--- include/atma/math/intersection.hpp
+++ include/atma/math/intersection.hpp
@@ -93,12 +93,9 @@ namespace atma { namespace math {
 		if (tmax.x < p.x || tmax.y < p.y || tmax.z < p.z || pm.x < tmin.x || pm.y < tmin.y || pm.z < tmin.z)
 			return false;
 
-		// triangle-normal & inward-facing edge normals
-		auto n = cross_product(tri.v1 - tri.v0, tri.v2 - tri.v0);
-		//auto ne0 = cross_product(tri.edge0(), n);
-		//auto ne1 = cross_product(tri.edge1(), n);
-		//auto ne2 = cross_product(tri.edge2(), n);
-
+		// triangle-normal
+		auto n = vector4f{cross_product(tri.edge0(), tri.edge1()).normalized()};
+		
 		// delta-p, the vector of (min-point, max-point) of the bounding-box
 		vector4f dp = box.max_point() - p;
 
@@ -112,78 +109,65 @@ namespace atma { namespace math {
 		auto d1 = dot_product(n, c - tri.v0);
 		auto d2 = dot_product(n, dp - c - tri.v0);
 
-		// test for plane-box overlap
+		// test for triangle-plane/box overlap
 		if ((dot_product(n, p) + d1) * (dot_product(n, p) + d2) > 0.f)
 			return false;
 
 
-		auto pxy = vector4f(p.x, p.y, 0.f, 0.f);
-		auto pxz = vector4f(p.x, p.z, 0.f, 0.f);
-		auto pyz = vector4f(p.y, p.z, 0.f, 0.f);
-
-		auto edge_normal = [](int a, int b, vector4f const& v0, vector4f const& v1, vector4f const& v2)
-		{
-			auto AB = vector4f {v1 - v0};
-			auto AC = vector4f {v2 - v0};
-			auto N = vector4f {-AB[b], AB[a], 0.f, 0.f};
-			if (v1[a]*v2[b] - v0[b]*v1[a] - v0[a]*v2[b] - v1[b]*v2[a] + v0[a]*v2[b] + v0[b]*v2[a] < 0.f)
-				N = N * -1;
-			return N;
-		};
-
-
 		// xy-plane projection-overlap
-		vector4f ne0xy = edge_normal(0,1, tri.v0, tri.v1, tri.v2);
-		vector4f ne1xy = edge_normal(0,1, tri.v1, tri.v2, tri.v0);
-		vector4f ne2xy = edge_normal(0,1, tri.v2, tri.v0, tri.v1);
+		vector4f ne0xy = vector4f{-tri.edge0().y, tri.edge0().x, 0.f, 0.f} * (n.z < 0.f ? -1.f : 1.f);
+		vector4f ne1xy = vector4f{-tri.edge1().y, tri.edge1().x, 0.f, 0.f} * (n.z < 0.f ? -1.f : 1.f);
+		vector4f ne2xy = vector4f{-tri.edge2().y, tri.edge2().x, 0.f, 0.f} * (n.z < 0.f ? -1.f : 1.f);
 
-		auto  v0xy   = math::vector4f{tri.v0.x, tri.v0.y, 0.f, 0.f};
-		auto  v1xy   = math::vector4f{tri.v1.x, tri.v1.y, 0.f, 0.f};
-		auto  v2xy   = math::vector4f{tri.v2.x, tri.v2.y, 0.f, 0.f};
+		auto v0xy = math::vector4f{tri.v0.x, tri.v0.y, 0.f, 0.f};
+		auto v1xy = math::vector4f{tri.v1.x, tri.v1.y, 0.f, 0.f};
+		auto v2xy = math::vector4f{tri.v2.x, tri.v2.y, 0.f, 0.f};
 
 		float de0xy = -dot_product(ne0xy, v0xy) + std::max(0.f, dp.x * ne0xy.x) + std::max(0.f, dp.y * ne0xy.y);
 		float de1xy = -dot_product(ne1xy, v1xy) + std::max(0.f, dp.x * ne1xy.x) + std::max(0.f, dp.y * ne1xy.y);
 		float de2xy = -dot_product(ne2xy, v2xy) + std::max(0.f, dp.x * ne2xy.x) + std::max(0.f, dp.y * ne2xy.y);
 
+		auto pxy = vector4f(p.x, p.y, 0.f, 0.f);
+
 		if ((dot_product(ne0xy, pxy) + de0xy) < 0.f || (dot_product(ne1xy, pxy) + de1xy) < 0.f || (dot_product(ne2xy, pxy) + de2xy) < 0.f)
 			return false;
 
 
-		// xz-plane projection overlap
-		vector4f ne0xz = edge_normal(0,2, tri.v0, tri.v1, tri.v2);
-		vector4f ne1xz = edge_normal(0,2, tri.v1, tri.v2, tri.v0);
-		vector4f ne2xz = edge_normal(0,2, tri.v2, tri.v0, tri.v1);
-
-		vector4f ne0xz2 = vector4f(-tri.edge0().z, tri.edge0().x, 0.f, 0.f) * (n.z < 0.f ? -1.f : 1.f);
-		vector4f ne1xz2 = vector4f(-tri.edge1().z, tri.edge1().x, 0.f, 0.f) * (n.z < 0.f ? -1.f : 1.f);
-		vector4f ne2xz2 = vector4f(-tri.edge2().z, tri.edge2().x, 0.f, 0.f) * (n.z < 0.f ? -1.f : 1.f);
-
-		auto  v0xz   = math::vector4f{tri.v0.x, tri.v0.z, 0.f, 0.f};
-		auto  v1xz   = math::vector4f{tri.v1.x, tri.v1.z, 0.f, 0.f};
-		auto  v2xz   = math::vector4f{tri.v2.x, tri.v2.z, 0.f, 0.f};
-
-		float de0xz = -dot_product(ne0xz, v0xz) + std::max(0.f, dp.x * ne0xz.x) + std::max(0.f, dp.z * ne0xz.y);
-		float de1xz = -dot_product(ne1xz, v1xz) + std::max(0.f, dp.x * ne1xz.x) + std::max(0.f, dp.z * ne1xz.y);
-		float de2xz = -dot_product(ne2xz, v2xz) + std::max(0.f, dp.x * ne2xz.x) + std::max(0.f, dp.z * ne2xz.y);
-
-		if ((dot_product(ne0xz, pxz) + de0xz) < 0.f || (dot_product(ne1xz, pxz) + de1xz) < 0.f || (dot_product(ne2xz, pxz) + de2xz) < 0.f)
-			return false;
-
-
 		// yz-plane projection overlap
-		vector4f ne0yz = edge_normal(1,2, tri.v0, tri.v1, tri.v2);
-		vector4f ne1yz = edge_normal(1,2, tri.v1, tri.v2, tri.v0);
-		vector4f ne2yz = edge_normal(1,2, tri.v2, tri.v0, tri.v1);
+		vector4f ne0yz = vector4f{-tri.edge0().z, tri.edge0().y, 0.f, 0.f} * (n.x < 0.f ? -1.f : 1.f);
+		vector4f ne1yz = vector4f{-tri.edge1().z, tri.edge1().y, 0.f, 0.f} * (n.x < 0.f ? -1.f : 1.f);
+		vector4f ne2yz = vector4f{-tri.edge2().z, tri.edge2().y, 0.f, 0.f} * (n.x < 0.f ? -1.f : 1.f);
 
-		auto  v0yz   = math::vector4f{tri.v0.y, tri.v0.z, 0.f, 0.f};
-		auto  v1yz   = math::vector4f{tri.v1.y, tri.v1.z, 0.f, 0.f};
-		auto  v2yz   = math::vector4f{tri.v2.y, tri.v2.z, 0.f, 0.f};
+		auto v0yz = math::vector4f{tri.v0.y, tri.v0.z, 0.f, 0.f};
+		auto v1yz = math::vector4f{tri.v1.y, tri.v1.z, 0.f, 0.f};
+		auto v2yz = math::vector4f{tri.v2.y, tri.v2.z, 0.f, 0.f};
 
 		float de0yz = -dot_product(ne0yz, v0yz) + std::max(0.f, dp.y * ne0yz.x) + std::max(0.f, dp.z * ne0yz.y);
 		float de1yz = -dot_product(ne1yz, v1yz) + std::max(0.f, dp.y * ne1yz.x) + std::max(0.f, dp.z * ne1yz.y);
 		float de2yz = -dot_product(ne2yz, v2yz) + std::max(0.f, dp.y * ne2yz.x) + std::max(0.f, dp.z * ne2yz.y);
 
+		auto pyz = vector4f(p.y, p.z, 0.f, 0.f);
+
 		if ((dot_product(ne0yz, pyz) + de0yz) < 0.f || (dot_product(ne1yz, pyz) + de1yz) < 0.f || (dot_product(ne2yz, pyz) + de2yz) < 0.f)
+			return false;
+
+
+		// zx-plane projection overlap
+		vector4f ne0zx = vector4f{-tri.edge0().x, tri.edge0().z, 0.f, 0.f} * (n.y < 0.f ? -1.f : 1.f);
+		vector4f ne1zx = vector4f{-tri.edge1().x, tri.edge1().z, 0.f, 0.f} * (n.y < 0.f ? -1.f : 1.f);
+		vector4f ne2zx = vector4f{-tri.edge2().x, tri.edge2().z, 0.f, 0.f} * (n.y < 0.f ? -1.f : 1.f);
+
+		auto v0zx   = math::vector4f{tri.v0.z, tri.v0.x, 0.f, 0.f};
+		auto v1zx   = math::vector4f{tri.v1.z, tri.v1.x, 0.f, 0.f};
+		auto v2zx   = math::vector4f{tri.v2.z, tri.v2.x, 0.f, 0.f};
+
+		float de0zx = -dot_product(ne0zx, v0zx) + std::max(0.f, dp.y * ne0zx.x) + std::max(0.f, dp.z * ne0zx.y);
+		float de1zx = -dot_product(ne1zx, v1zx) + std::max(0.f, dp.y * ne1zx.x) + std::max(0.f, dp.z * ne1zx.y);
+		float de2zx = -dot_product(ne2zx, v2zx) + std::max(0.f, dp.y * ne2zx.x) + std::max(0.f, dp.z * ne2zx.y);
+
+		auto pzx = vector4f(p.z, p.x, 0.f, 0.f);
+
+		if ((dot_product(ne0zx, pzx) + de0zx) < 0.f || (dot_product(ne1zx, pzx) + de1zx) < 0.f || (dot_product(ne2zx, pzx) + de2zx) < 0.f)
 			return false;
 
 
