1926791bbd2530ae3ce1a0bbd1012b7bb41d1511
jona <omnigoat@gmail.com>
2015-03-10 00:15:11 +1100

    refactoring aabb/aabc/intersection

--- include/atma/math/intersection.hpp
+++ include/atma/math/intersection.hpp
@@ -7,78 +7,8 @@
 
 namespace atma { namespace math {
 
-	inline auto intersect_aabb_triangle(aabb_t const& aabb, triangle_t const& tri) -> bool
-	{
-		auto project = [](vector4f const& axis, vector4f const* points, size_t point_count, float& min, float& max) {
-			min = FLT_MAX;
-			max = -FLT_MAX;
-
-			for (auto p = points; p != points + point_count; ++p) {
-				float v = math::dot_product(axis, *p);
-				if (v < min) min = v;
-				if (v > max) max = v;
-			}
-		};
-
-
-		float box_min = 0.f, box_max = 0.f;
-		float tri_min = 0.f, tri_max = 0.f;
-
-		math::vector4f const tri_edges[3] ={
-			tri.edge0(),
-			tri.edge1(),
-			tri.edge2()
-		};
-
-		auto box_verts = aabb.compute_corners();
-
-		math::vector4f const box_normals[3] ={
-			math::vector4f{1.f, 0.f, 0.f, 1.f},
-			math::vector4f{0.f, 1.f, 0.f, 1.f},
-			math::vector4f{0.f, 0.f, 1.f, 1.f},
-		};
-
-
-		// test box normals
-		{
-			for (int i = 0; i != 3; ++i)
-			{
-				project(box_normals[i], &tri.v0, 3, tri_min, tri_max);
-				if (tri_max < aabb.origin().components[i] - aabb.extents().components[i] || tri_min > aabb.origin().components[i] + aabb.extents().components[i])
-					return false;
-			}
-		}
-
-		// test the triangle normal
-		{
-			auto tri_normal = math::cross_product(tri.v1 - tri.v0, tri.v2 - tri.v0);
-			auto tri_offset = math::dot_product(tri_normal, tri.v0);
-
-			project(tri_normal, &box_verts[0], 8, box_min, box_max);
-
-			if (box_max < tri_offset || box_min > tri_offset)
-				return false;
-		}
-
-
-		// test nine-edges
-		for (int i = 0; i != 3; ++i)
-		{
-			for (int j = 0; j != 3; ++j)
-			{
-				auto axis = math::cross_product(tri_edges[i], box_normals[j]);
-				project(axis, &box_verts[0], 8, box_min, box_max);
-				project(axis, &tri.v0, 3, tri_min, tri_max);
-				if (box_max <= tri_min || box_min >= tri_max)
-					return false;
-			}
-		}
-
-
-		return true;
-	}
-
-	inline auto intersect_aabc_triangle2(aabc_t const& box, triangle_t const& tri) -> bool
+	template <typename Box>
+	inline auto intersect_aabb_triangle(Box const& box, triangle_t const& tri) -> bool
 	{
 		auto p  = box.min_point();
 		auto pm = box.max_point();
@@ -99,17 +29,15 @@ namespace atma { namespace math {
 		// delta-p, the vector of (min-point, max-point) of the bounding-box
 		vector4f dp = box.max_point() - p;
 
-		// critical-point
+		// test for triangle-plane/box overlap
 		auto c = point4f(
 			n.x > 0.f ? dp.x : 0.f,
 			n.y > 0.f ? dp.y : 0.f,
 			n.z > 0.f ? dp.z : 0.f);
 
-		// d1 & d2
 		auto d1 = dot_product(n, c - tri.v0);
 		auto d2 = dot_product(n, dp - c - tri.v0);
 
-		// test for triangle-plane/box overlap
 		if ((dot_product(n, p) + d1) * (dot_product(n, p) + d2) > 0.f)
 			return false;
 
@@ -157,9 +85,9 @@ namespace atma { namespace math {
 		vector4f ne1zx = vector4f{-tri.edge1().x, tri.edge1().z, 0.f, 0.f} * (n.y < 0.f ? -1.f : 1.f);
 		vector4f ne2zx = vector4f{-tri.edge2().x, tri.edge2().z, 0.f, 0.f} * (n.y < 0.f ? -1.f : 1.f);
 
-		auto v0zx   = math::vector4f{tri.v0.z, tri.v0.x, 0.f, 0.f};
-		auto v1zx   = math::vector4f{tri.v1.z, tri.v1.x, 0.f, 0.f};
-		auto v2zx   = math::vector4f{tri.v2.z, tri.v2.x, 0.f, 0.f};
+		auto v0zx = math::vector4f{tri.v0.z, tri.v0.x, 0.f, 0.f};
+		auto v1zx = math::vector4f{tri.v1.z, tri.v1.x, 0.f, 0.f};
+		auto v2zx = math::vector4f{tri.v2.z, tri.v2.x, 0.f, 0.f};
 
 		float de0zx = -dot_product(ne0zx, v0zx) + std::max(0.f, dp.y * ne0zx.x) + std::max(0.f, dp.z * ne0zx.y);
 		float de1zx = -dot_product(ne1zx, v1zx) + std::max(0.f, dp.y * ne1zx.x) + std::max(0.f, dp.z * ne1zx.y);
@@ -175,9 +103,9 @@ namespace atma { namespace math {
 	}
 
 
-	inline auto intersect_aabc_triangle(aabc_t const& aabb, triangle_t const& tri) -> bool
+	inline auto intersect_aabc_triangle(aabc_t const& aabc, triangle_t const& tri) -> bool
 	{
-		return intersect_aabb_triangle(aabb_t {aabb.origin(), 0.5f * vector4f{aabb.radius(), aabb.radius(), aabb.radius(), 0.f}}, tri);
+		return intersect_aabb_triangle(aabc, tri);
 	}
 
 } }
