50f9cc8f2a2444f9bd3c51e0234efb8b78c139a0
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-03-14 15:31:44 +1100

    removed ET and SSE-ifdef from vector4f
     - ETs don't really make sense for register-based XMM data, and
       we can't use auto for our ETs :(
    
     - SSE is supported on any CPU since 99, so we don't need to
       conditionally compile with this.

--- include/atma/math/vector4f.hpp
+++ include/atma/math/vector4f.hpp
@@ -1,16 +1,179 @@
 #pragma once
 
-#define ATMA_MATH_VECTOR4F_SCOPE
+#include <atma/types.hpp>
+
+#pragma warning(push,3)
+#include <xmmintrin.h>
+#pragma warning(pop)
 
 #include <cstdint>
-#include <atma/types.hpp>
-#include <atma/math/impl/vector4f_fwd.hpp>
-#include <atma/math/impl/expr.hpp>
-#include <atma/math/impl/vector4f_declaration.hpp>
-#include <atma/math/impl/vector4f_exprs.hpp>
-#include <atma/math/impl/vector4f_definition.hpp>
-#include <atma/math/impl/vector4f_operators.hpp>
 
-namespace aml = atma::math;
 
-#undef ATMA_MATH_VECTOR4F_SCOPE
+namespace atma { namespace math {
+
+	__declspec(align(16))
+	struct vector4f
+	{
+		vector4f();
+		explicit vector4f(__m128);
+		vector4f(float x, float y, float z);
+		vector4f(float x, float y, float z, float w);
+		vector4f(vector4f const&) = default;
+
+		auto operator = (vector4f const&) -> vector4f&;
+		
+		auto is_zero() const -> bool;
+		auto magnitude_squared() const -> float;
+		auto magnitude() const -> float;
+
+		// mutation
+		auto operator += (vector4f const&) -> vector4f&;
+		auto operator -= (vector4f const&) -> vector4f&;
+		auto operator *= (float) -> vector4f&;
+		auto operator /= (float) -> vector4f&;
+
+#pragma warning(push)
+#pragma warning(disable: 4201)
+		union
+		{
+			__m128 xmmdata;
+
+			float components[4];
+
+			struct {
+				float x, y, z, w;
+			};
+		};
+#pragma warning(pop)
+	};
+
+
+
+
+	//=====================================================================
+	// implementation
+	//=====================================================================
+	inline vector4f::vector4f()
+		: xmmdata(_mm_setzero_ps())
+	{
+	}
+
+	inline vector4f::vector4f(float x, float y, float z, float w)
+	{
+		__declspec(align(16)) float fs[] ={x, y, z, w};
+		xmmdata = _mm_load_ps(fs);
+	}
+
+	inline vector4f::vector4f(__m128 xm)
+		: xmmdata(xm)
+	{
+	}
+
+	inline auto vector4f::operator = (vector4f const& rhs) -> vector4f&
+	{
+		xmmdata = rhs.xmmdata;
+		return *this;
+	}
+
+	inline auto vector4f::magnitude() const -> float
+	{
+		return _mm_sqrt_ss(_mm_dp_ps(xmmdata, xmmdata, 0x7f)).m128_f32[0];
+	}
+
+	inline auto vector4f::magnitude_squared() const -> float
+	{
+		return _mm_dp_ps(xmmdata, xmmdata, 0x7f).m128_f32[0];
+	}
+
+	inline auto vector4f::operator += (vector4f const& rhs) -> vector4f&
+	{
+		xmmdata = _mm_add_ps(xmmdata, rhs.xmmdata);
+		return *this;
+	}
+
+	inline auto vector4f::operator -= (vector4f const& rhs) -> vector4f&
+	{
+		xmmdata = _mm_sub_ps(xmmdata, rhs.xmmdata);
+		return *this;
+	}
+
+	inline auto vector4f::operator *= (float rhs) -> vector4f&
+	{
+		xmmdata = _mm_mul_ps(xmmdata, _mm_load_ps1(&rhs));
+		return *this;
+	}
+
+	inline auto vector4f::operator /= (float rhs) -> vector4f&
+	{
+		xmmdata = _mm_div_ps(xmmdata, _mm_load_ps1(&rhs));
+		return *this;
+	}
+
+
+
+
+	//=====================================================================
+	// operators
+	//=====================================================================
+	inline auto operator + (vector4f const& lhs, vector4f const& rhs) -> vector4f
+	{
+		return vector4f{_mm_add_ps(lhs.xmmdata, rhs.xmmdata)};
+	}
+
+
+	inline auto operator - (vector4f const& lhs, vector4f const& rhs) -> vector4f
+	{
+		return vector4f{_mm_sub_ps(lhs.xmmdata, rhs.xmmdata)};
+	}
+
+
+	inline auto operator * (vector4f const& lhs, float rhs) -> vector4f
+	{
+		return vector4f{_mm_mul_ps(lhs.xmmdata, _mm_load_ps1(&rhs))};
+	}
+
+	inline auto operator * (float lhs, vector4f const& rhs) -> vector4f
+	{
+		return vector4f{_mm_mul_ps(_mm_load_ps1(&lhs), rhs.xmmdata)};
+	}
+
+
+	inline auto operator / (vector4f const& lhs, float rhs) -> vector4f
+	{
+		return vector4f{_mm_div_ps(lhs.xmmdata, _mm_load_ps1(&rhs))};
+	}
+
+	inline auto operator / (float lhs, vector4f const& rhs) -> vector4f
+	{
+		return vector4f{_mm_div_ps(_mm_load_ps1(&lhs), rhs.xmmdata)};
+	}
+
+
+
+
+	//=====================================================================
+	// functions
+	//=====================================================================
+	inline auto normalize(vector4f const& x) -> vector4f
+	{
+		return vector4f{_mm_mul_ps(x.xmmdata, _mm_rsqrt_ps(_mm_dp_ps(x.xmmdata, x.xmmdata, 0x7f)))};
+	}
+
+	inline auto dot_product(vector4f const& lhs, vector4f const& rhs) -> float
+	{
+		return _mm_dp_ps(lhs.xmmdata, rhs.xmmdata, 0x7f).m128_f32[0];
+	}
+
+	inline auto cross_product(vector4f const& lhs, vector4f const& rhs) -> vector4f
+	{
+		return vector4f(
+			_mm_sub_ps(
+				_mm_mul_ps(
+					_mm_shuffle_ps(lhs.xmmdata, lhs.xmmdata, _MM_SHUFFLE(3, 0, 2, 1)),
+					_mm_shuffle_ps(rhs.xmmdata, rhs.xmmdata, _MM_SHUFFLE(3, 1, 0, 2))),
+				_mm_mul_ps(
+					_mm_shuffle_ps(lhs.xmmdata, lhs.xmmdata, _MM_SHUFFLE(3, 1, 0, 2)),
+					_mm_shuffle_ps(rhs.xmmdata, rhs.xmmdata, _MM_SHUFFLE(3, 0, 2, 1)))));
+	}
+
+} }
