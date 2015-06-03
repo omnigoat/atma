b518decce80393f9d8daa3c1232f4995414d7c10
omnigoat <379771+omnigoat@users.noreply.github.com>
2015-06-03 17:22:37 +1000

    changes to hashing

--- include/atma/hash.hpp
+++ include/atma/hash.hpp
@@ -2,56 +2,25 @@
 
 namespace atma
 {
+	template <typename T>
+	struct hash_t
+	{
+		auto operator()(hasher_t& hsh, T const& x) const -> void
+		{
+			return hsh(&x, sizeof(T));
+		}
+	};
+
 	struct hasher_t
 	{
-		hasher_t(uint64 seed = 0)
-			: hash_(seed)
-			, tail_()
-			, count_()
-			, size_()
-		{
-		}
+		hasher_t(uint64 seed = 0);
 
 		template <typename T>
-		auto operator ()(T const& t) -> hasher_t&
-		{
-			(*this)(&t, sizeof(T));
-			return *this;
-		}
+		auto operator ()(T const& t) -> hasher_t&;
 
-		auto operator ()(void const* datav, size_t size) -> hasher_t&
-		{
-			auto data = reinterpret_cast<uchar const*>(datav);
+		auto operator ()(void const* datav, size_t size) -> hasher_t&;
 
-			size_ += size;
-
-			mix_tail(data, size);
-
-			while (size >= 8)
-			{
-				uint64 k = *(uint64*)data;
-
-				mmix(hash_, k);
-
-				data += 8;
-				size -= 8;
-			}
-
-			mix_tail(data, size);
-			return *this;
-		}
-
-		uint64 result()
-		{
-			mmix(hash_, tail_);
-			mmix(hash_, size_);
-
-			hash_ ^= hash_ >> r;
-			hash_ *= m;
-			hash_ ^= hash_ >> r;
-
-			return hash_;
-		}
+		auto result() -> uint64;
 
 	private:
 		auto mmix(uint64& h, uint64 k) const -> void
@@ -109,12 +78,56 @@ namespace atma
 		}
 	};
 
-	template <typename T>
-	struct hash_t
+
+
+
+
+	inline hasher_t::hasher_t(uint64 seed)
+		: hash_(seed)
+		, tail_()
+		, count_()
+		, size_()
 	{
-		auto operator()(T const& x) const -> size_t
+	}
+
+	inline auto hasher_t::operator ()(void const* datav, size_t size) -> hasher_t&
+	{
+		auto data = reinterpret_cast<uchar const*>(datav);
+
+		size_ += size;
+
+		mix_tail(data, size);
+
+		while (size >= 8)
 		{
-			return hasher_t()(&x, sizeof(T)).result();
+			uint64 k = *(uint64*)data;
+
+			mmix(hash_, k);
+
+			data += 8;
+			size -= 8;
 		}
-	};
+
+		mix_tail(data, size);
+		return *this;
+	}
+
+	inline auto hasher_t::result() -> uint64
+	{
+		mmix(hash_, tail_);
+		mmix(hash_, size_);
+
+		hash_ ^= hash_ >> r;
+		hash_ *= m;
+		hash_ ^= hash_ >> r;
+
+		return hash_;
+	}
+
+	template <typename T>
+	inline auto hasher_t::operator ()(T const& t) -> hasher_t&
+	{
+		hash_t<T>()(*this, t);
+		return *this;
+	}
 }
