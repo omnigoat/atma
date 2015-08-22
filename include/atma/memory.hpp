1bb564414186acff82cd34a90b079b8fb0c805c5
jona <omnigoat@gmail.com>
2015-08-22 15:28:49 +1000

    memory_t refactor. now it's best

--- include/atma/memory.hpp
+++ include/atma/memory.hpp
@@ -25,21 +25,21 @@ namespace atma
 			typename Alloc,
 			bool Empty = std::is_empty<Alloc>::value
 		>
-		struct base_memory_t;
+		struct base_memory_tx;
 
 
 		template <typename Alloc>
-		struct base_memory_t<Alloc, false>
+		struct base_memory_tx<Alloc, false>
 		{
-			base_memory_t()
+			base_memory_tx()
 			{}
 
 			template <typename U>
-			base_memory_t(base_memory_t<U> const& rhs)
+			base_memory_tx(base_memory_tx<U> const& rhs)
 				: allocator_(rhs.allocator())
 			{}
 
-			base_memory_t(Alloc const& allocator)
+			base_memory_tx(Alloc const& allocator)
 				: allocator_(allocator)
 			{}
 
@@ -52,55 +52,44 @@ namespace atma
 
 
 		template <typename Alloc>
-		struct base_memory_t<Alloc, true>
+		struct base_memory_tx<Alloc, true>
 			: protected Alloc
 		{
-			base_memory_t()
+			base_memory_tx()
 			{}
 
 			template <typename U>
-			base_memory_t(base_memory_t<U> const& rhs)
+			base_memory_tx(base_memory_tx<U> const& rhs)
 				: Alloc(rhs.allocator())
 			{}
 
-			base_memory_t(Alloc const& allocator)
+			base_memory_tx(Alloc const& allocator)
 				: Alloc(allocator)
 			{}
 
 			auto allocator() -> Alloc& { return static_cast<Alloc&>(*this); }
 			auto allocator() const -> Alloc const& { return static_cast<Alloc const&>(*this); }
 		};
+
+
+		template <typename T, typename A>
+		using base_memory_t = base_memory_tx<typename A::template rebind<T>::other>;
 	}
 
 
 
 
 	template <typename T, typename Allocator>
-	struct memory_t : detail::base_memory_t<typename Allocator::template rebind<T>::other>
+	struct memory_t : detail::base_memory_t<T, Allocator>
 	{
 		using value_type = T;
 
 		memory_t();
-		memory_t(memory_t&&);
-		template <typename U> explicit memory_t(memory_t<T, U> const&);
+		template <typename B> memory_t(memory_t<T, B> const&);
 		explicit memory_t(Allocator const& allocator);
 		explicit memory_t(value_type* data, Allocator const& = Allocator());
-		memory_t(memory_t const&) = delete;
 
-		auto operator = (memory_t const&) -> memory_t& = delete;
-
-		auto alloc(size_t capacity) -> void;
-		auto deallocate() -> void;
-		auto construct_default(size_t offset, size_t count, value_type const& = value_type()) -> void;
-		auto construct_copy(size_t offset, value_type const&) -> void;
-		auto construct_copy_range(size_t offset, value_type const*, size_t size) -> void;
-		auto construct_move(size_t offset, value_type&&) -> void;
-		auto destruct(size_t offset, size_t count) -> void;
-
-		auto memmove(size_t dest, size_t src, size_t count) -> void;
-		template <typename U> auto memcpy(size_t dest, memory_t<T,U> const&, size_t src, size_t count) -> void;
-
-		auto detach_ptr() -> value_type*;
+		template <typename B> auto operator = (memory_t<T, B> const&) -> memory_t&;
 
 		value_type* ptr;
 	};
@@ -115,13 +104,14 @@ namespace atma
 
 	template <typename T, typename A>
 	inline memory_t<T,A>::memory_t(A const& allocator)
-		: detail::base_memory_t(allocator)
+		: detail::base_memory_t<T, A>(allocator)
 		, ptr()
 	{}
 
 	template <typename T, typename A>
-	inline memory_t<T,A>::memory_t(memory_t&& rhs)
-		: detail::base_memory_t<A>(rhs)
+	template <typename B>
+	inline memory_t<T,A>::memory_t(memory_t<T, B> const& rhs)
+		: detail::base_memory_t<T, A>(rhs.allocator())
 		, ptr(rhs.ptr)
 	{
 		rhs.ptr = nullptr;
@@ -129,73 +119,84 @@ namespace atma
 
 	template <typename T, typename A>
 	inline memory_t<T,A>::memory_t(value_type* data, A const& alloc)
-		: detail::base_memory_t<A>(alloc)
+		: detail::base_memory_t<T, A>(alloc)
 		, ptr(data)
 	{}
 
 	template <typename T, typename A>
-	inline auto memory_t<T,A>::alloc(size_t capacity) -> void
+	template <typename B>
+	inline auto memory_t<T, A>::operator = (memory_t<T, B> const& rhs) -> memory_t&
 	{
-		ptr = allocator().allocate(capacity);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::deallocate() -> void
-	{
-		return allocator().deallocate(ptr, 0);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::construct_default(size_t offset, size_t count, value_type const& x) -> void
-	{
-		for (auto i = offset; i != offset + count; ++i)
-			allocator().construct(ptr + i, x);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::construct_copy(size_t offset, value_type const& x) -> void
-	{
-		allocator().construct(ptr + offset, x);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::construct_copy_range(size_t offset, value_type const* rhs, size_t size) -> void
-	{
-		for (auto i = size_t{}, j = offset; i != size; ++i, ++j)
-			allocator().construct(ptr + j, rhs[i]);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::construct_move(size_t offset, value_type&& rhs) -> void
-	{
-		allocator().construct(ptr + offset, std::move(rhs));
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::destruct(size_t offset, size_t count) -> void
-	{
-		for (auto i = offset; i != offset + count; ++i)
-			allocator().destroy(ptr + i);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::memmove(size_t dest, size_t src, size_t count) -> void
-	{
-		std::memmove(ptr + dest, ptr + src, sizeof(value_type) * count);
-	}
-
-	template <typename T, typename A>
-	template <typename U>
-	inline auto memory_t<T,A>::memcpy(size_t dest, memory_t<T, U> const& rhs, size_t src, size_t count) -> void
-	{
-		std::memcpy(ptr + dest, rhs.ptr + src, sizeof(T) * count);
-	}
-
-	template <typename T, typename A>
-	inline auto memory_t<T,A>::detach_ptr() -> value_type*
-	{
-		auto t = ptr;
 		ptr = nullptr;
-		return t;
+		allocator() = rhs.allocator();
+		return *this;
+	}
+
+
+
+
+
+
+
+
+
+
+
+	template <typename T, typename A>
+	inline auto memory_allocate(memory_t<T, A>& dest, size_t capacity) -> void
+	{
+		dest.ptr = dest.allocator().allocate(capacity);
+	}
+
+	template <typename T, typename A>
+	inline auto memory_deallocate(memory_t<T, A>& dest) -> void
+	{
+		return dest.allocator().deallocate(dest.ptr, 0);
+	}
+
+	template <typename T, typename A>
+	inline auto memory_construct_default(memory_t<T, A>& dest, size_t offset, size_t count) -> void
+	{
+		for (auto i = offset; i != offset + count; ++i)
+			dest.allocator().construct(dest.ptr + i);
+	}
+
+	template <typename T, typename A>
+	inline auto memory_construct_copy(memory_t<T, A>& dest, size_t offset, size_t count, T const& x) -> void
+	{
+		for (auto i = offset; i != offset + count; ++i)
+			dest.allocator().construct(dest.ptr + i, x);
+	}
+
+	template <typename T, typename A>
+	inline auto memory_construct_copy(memory_t<T, A>& dest, size_t offset, size_t count, T const* src) -> void
+	{
+		for (auto i = size_t(), j = offset; i != count; ++i, ++j)
+			dest.allocator().construct(dest.ptr + j, src[i]);
+	}
+
+	template <typename T, typename A>
+	inline auto memory_construct_move(memory_t<T, A>& dest, size_t offset, T&& x) -> void
+	{
+		dest.allocator().construct(dest.ptr + offset, std::move(x));
+	}
+
+	template <typename T, typename A>
+	inline auto memory_destruct(memory_t<T, A>& dest, size_t offset, size_t count) -> void
+	{
+		for (auto i = offset; i != offset + count; ++i)
+			dest.allocator().destroy(dest.ptr + i);
+	}
+
+	template <typename T, typename A>
+	inline auto memory_memmove(memory_t<T, A>& dest, size_t dest_idx, size_t src_idx, size_t count) -> void
+	{
+		std::memmove(dest.ptr + dest_idx, dest.ptr + src_idx, sizeof(T) * count);
+	}
+
+	template <typename T, typename A, typename Y, typename B>
+	inline auto memory_memcpy(memory_t<T, A>& dest, size_t dest_idx, memory_t<Y, B> const& src, size_t src_idx, size_t count) -> void
+	{
+		std::memcpy(dest.ptr + dest_idx, reinterpret_cast<T const*>(src.ptr + src_idx), sizeof(T) * count);
 	}
 }
