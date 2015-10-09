96661fbffec317620f8237047a641b96d1c8ff71
omnigoat <omnigoat@gmail.com>
2015-10-09 16:19:58 +1100

    add more char methods

--- include/atma/utf/utf8_char.hpp
+++ include/atma/utf/utf8_char.hpp
@@ -1,34 +1,37 @@
 #pragma once
-//=====================================================================
+
 #include <atma/types.hpp>
 #include <atma/assert.hpp>
+
+
 //=====================================================================
-namespace atma {
+//
+//  overview of terminology
+//  =========================
+//
+//    utf8_byte_...
+//    ---------------
+//      the utf8_byte prefix relates to bytes in a utf8 string. any
+//      bytes. this is different from chars.
+//
+//    utf8_char_...
+//    ---------------
+//      utf8_char prefixes anything that deals with a sequence of bytes
+//      that make up a multibyte utf8 codepoint. it is assumed that
+//      any utf8_char function is operating on data aligned to a
+//      *leading* utf8 byte. a.k.a: at the beginning of a sequence.
+//
+//      sometimes these functions accept a single char. whatever.
+//
+//    utf8_charseq_...
+//    ------------------
+//      utf8_charseq signifies a sequence of utf8-chars, basically a
+//      raw-string. charseqs are used for algorithms, mainly.
+//
 //=====================================================================
-
-
-	//=====================================================================
-	//
-	//  overview of terminology
-	//  =========================
-	//
-	//    utf8_byte_...
-	//    ---------------
-	//      the utf8_byte prefix relates to bytes in a utf8 string. any
-	//      bytes. this is different from chars.
-	//
-	//    utf8_char_...
-	//    ---------------
-	//      utf8_char prefixes anything that deals with a sequence of bytes
-	//      that make up a multibyte utf8 codepoint. it is assumed that
-	//      any utf8_char function is operating on data aligned to a
-	//      *leading* utf8 byte. a.k.a: at the beginning of a sequence.
-	//
-	//      sometimes these functions accept a single char. whatever.
-	//
-	//=====================================================================
-
-
+namespace atma
+{
+	using codepoint = uint32;
 
 	namespace detail
 	{
@@ -78,13 +81,17 @@ namespace atma {
 		return detail::char_length_table[c] >= 1;
 	}
 
-	// c in [0, 128)
+	inline auto utf8_byte_is_run_on(char const c) -> bool {
+		return detail::char_length_table[c] == 0;
+	}
+
+	// c in codepoint [0, 128), a.k.a: ascii
 	inline auto utf8_char_is_ascii(char const c) -> bool {
 		return detail::char_length_table[c] == 1;
 	}
 
 	// return how many bytes we need to advance, assuming we're at a leading byte
-	inline auto utf8_char_bytecount(char const* leading) -> int {
+	inline auto utf8_char_bytecount(char const* leading) -> int8 {
 		ATMA_ASSERT(leading);
 		ATMA_ENSURE(utf8_byte_is_leading(*leading));
 		return detail::char_length_table[*leading];
@@ -94,88 +101,75 @@ namespace atma {
 		return leading + utf8_char_bytecount(leading);
 	}
 
-
-	//=====================================================================
-	// utf8_char_t
-	// -------------
-	//   a sequence of bytes that make up a utf8 character encoding.
-	//
-	//   there is no "null" state for this byte, the default state is the
-	//   codepoint zero.
-	//=====================================================================
-	struct utf8_char_t
+	inline auto utf8_char_equality(char const* lhs, char const* rhs) -> bool
 	{
-		utf8_char_t()
-			: begin(&detail::zero_byte), end(&detail::zero_byte + 1)
-		{}
-
-		utf8_char_t(char const* begin, char const* end)
-			: begin(begin), end(end)
-		{
-			ATMA_ASSERT(begin);
-			ATMA_ASSERT(end);
-		}
-
-		utf8_char_t(utf8_char_t const& rhs)
-			: begin(rhs.begin), end(rhs.end)
-		{
-			ATMA_ASSERT(begin);
-			ATMA_ASSERT(end);
-		}
-
-
-		auto operator = (utf8_char_t const& rhs) -> utf8_char_t&
-		{
-			begin = rhs.begin;
-			end = rhs.end;
-			return *this;
-		}
-
-		char const* begin;
-		char const* end;
-	};
-
-	inline auto operator == (utf8_char_t const& lhs, utf8_char_t const& rhs) -> bool {
-		return (lhs.end - lhs.begin) == (rhs.end - rhs.begin)
-			&& std::equal(lhs.begin, lhs.end, rhs.begin)
-			;
-	}
-
-	inline auto operator == (utf8_char_t const& lhs, char x) -> bool {
-		// it only makes sense to compare a character against a character, and
-		// not a character against a leading-byte in a char-seq.
-		ATMA_ASSERT(utf8_char_is_ascii(x));
-
-		return *lhs.begin == x;
-	}
-
-
-	inline auto utf8_char_equality(utf8_char_t const& lhs, char const* rhs) -> bool
-	{
-		if (rhs == nullptr)
-			return false;
-
+		ATMA_ASSERT(lhs);
+		ATMA_ASSERT(rhs);
+		ATMA_ASSERT(utf8_byte_is_leading(*lhs));
 		ATMA_ASSERT(utf8_byte_is_leading(*rhs));
 
-		for (int i = 0, end = utf8_char_bytecount(lhs.begin); i != end && *rhs; ++i, ++rhs)
-			if (lhs.begin[i] != *rhs)
+		if (*lhs != *rhs)
+			return false;
+
+		for (int8 i = 1, end = utf8_char_bytecount(lhs); i != end; ++i)
+			if (lhs[i] != rhs[i])
 				return false;
 		
 		return true;
 	}
 
+	inline auto utf8_char_codepoint(char const* x) -> codepoint
+	{
+		ATMA_ASSERT(x);
+		ATMA_ASSERT(utf8_byte_is_leading(*x));
+
+		codepoint r = 0;
+
+		if ((x[0] & 0x80) == 0)
+			r = *x;
+		else if ((x[0] & 0xe0) == 0xc0)
+			r = ((x[0] & 0x1f) << 6) | (x[1] & 0x3f);
+		else if ((x[0] & 0xf0) == 0xe0)
+			r = ((x[0] & 0x0f) << 12) | ((x[1] & 0x3f) << 6) | (x[2] & 0x3f);
+		else if ((x[0] & 0xf8) == 0xf0)
+			r = ((x[0] & 0x07) << 18) | ((x[1] & 0x3f) << 12) | ((x[2] & 0x3f) << 6) | ((x[3] & 0x3f);
+
+		return r;
+	}
+
+	template <typename FN>
+	inline auto utf8_charseq_for_each(char const* seq, FN&& fn) -> void
+	{
+		ATMA_ASSERT(seq);
+		ATMA_ASSERT(utf8_byte_is_leading(*seq));
+
+		for (auto i = seq; *i; i = utf8_char_advance(i))
+			fn(i);
+	}
+
 	template <typename T>
 	inline auto utf8_charseq_any_of(char const* seq, T&& pred) -> bool
 	{
+		ATMA_ASSERT(seq);
+		ATMA_ASSERT(utf8_byte_is_leading(*seq));
+
 		for (auto i = seq; *i; i = utf8_char_advance(i))
-			if (pred(utf8_char_t{i, utf8_char_advance(i)}))
+			if (pred(i))
 				return true;
 
 		return false;
 	}
 
+	template <typename T>
+	inline auto utf8_charseq_find(char const* seq, char const* x) -> char const*
+	{
+		ATMA_ASSERT(seq);
+		ATMA_ASSERT(utf8_byte_is_leading(*seq));
 
+		for (auto i = seq; *i; i = utf8_char_advance(i))
+			if (utf8_char_equality(x, i))
+				return i;
 
-//=====================================================================
-} // namespace atma
-//=====================================================================
+		return nullptr;
+	}
+}
