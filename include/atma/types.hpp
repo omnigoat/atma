c817ab6720f76c7df2e55189172c7c5c34096792
jona <omnigoat@gmail.com>
2015-10-18 00:30:21 +1100

    add transfer_const_t

--- include/atma/types.hpp
+++ include/atma/types.hpp
@@ -24,3 +24,14 @@ typedef uint64_t uint64;
 typedef intptr_t intptr;
 typedef uintptr_t uintptr;
 
+
+namespace atma
+{
+	//
+	//  transfer_const_t
+	//  ------------------
+	//    takes T, and if const, transforms M to be const, otherwise just M
+	//
+	template <typename T, typename M>
+	using transfer_const_t = std::conditional_t<std::is_const<T>::value, M const, M>;
+}
