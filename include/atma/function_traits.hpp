8479c6d3230061bf22e071d129a4d69b874e7634
omnigoat <379771+omnigoat@users.noreply.github.com>
2016-04-03 13:34:51 +1000

    refactored function-traits & friends

--- include/atma/function_traits.hpp
+++ include/atma/function_traits.hpp
@@ -13,31 +13,16 @@ namespace atma
 	namespace detail
 	{
 		// SFINAE test
-#if 0
 		template <typename T>
 		struct has_functor_operator_t
 		{
 		private:
-			template <typename C> constexpr static bool test(decltype(&C::helloworld)) { return true; }
+			template <typename C> constexpr static bool test(decltype(&C::operator ())) { return true; }
 			template <typename C> constexpr static bool test(...) { return false; }
 
 		public:
 			constexpr static bool value = test<T>(0);
 		};
-#endif
-		template <typename T>
-		class has_functor_operator_t
-		{
-			typedef char one;
-			typedef long two;
-
-			template <typename C> static one test(decltype(&C::operator ()));
-			template <typename C> static two test(...);
-
-		public:
-			enum { value = sizeof(test<T>(0)) == sizeof(char) };
-		};
-
 	}
 
 	template <typename T>
@@ -45,119 +30,105 @@ namespace atma
 
 
 	//
-	//  is_callable_v
+	//  is_function_pointer_v
+	//  -----------------------
+	//    srsly, c++ std.
 	//
 	namespace detail
 	{
 		template <typename T>
-		struct is_callable_t
-			: has_functor_operator_t<T>
+		struct is_function_pointer_tx
+			: std::false_type
 		{};
 
 		template <typename R, typename... Args>
-		struct is_callable_t<R(*)(Args...)> {
-			constexpr static bool value = true;
+		struct is_function_pointer_tx<R(*)(Args...)>
+			: std::true_type
+		{};
+	}
+
+	template <typename T>
+	constexpr bool is_function_pointer_v = detail::is_function_pointer_tx<T>::value;
+
+
+	//
+	//  is_callable_v
+	//  ---------------
+	//    returns true for a function-pointer, a member-function-pointer, or a functor
+	//
+	template <typename T>
+	constexpr bool is_callable_v =
+		is_function_pointer_v<T> || std::is_member_function_pointer_v<T> || has_functor_operator_v<T>;
+
+
+
+
+
+
+	//
+	//  function_traits
+	//  -----------------
+	//    good ol' function traits
+	//
+	namespace detail
+	{
+		// functor
+		template <typename T>
+		struct function_traits_tx
+			: function_traits_tx<decltype(&T::operator())>
+		{
+			// subtle interaction: a functor is *not* a member-function-pointer,
+			// even though we derive the majority of our definition from the member-
+			// function-pointer of its call-operator
+			static bool const is_memfnptr = false;
 		};
 
-		template <typename R, typename C, typename... Args>
-		struct is_callable_t<R(C::*)(Args...)> {
-			constexpr static bool value = true;
+		// function-pointer
+		template <typename R, typename... Args>
+		struct function_traits_tx<R(*)(Args...)>
+			: function_traits_tx<R(Args...)>
+		{};
+
+		// member-function-pointer
+		template <typename C, typename R, typename... Args>
+		struct function_traits_tx<R(C::*)(Args...) const>
+			: function_traits_tx<R(Args...)>
+		{
+			constexpr static bool is_memfnptr = true;
+			using class_type = C;
 		};
 
-		template <typename R, typename C, typename... Args>
-		struct is_callable_t<R(C::*)(Args...) const> {
-			constexpr static bool value = true;
+		template <typename C, typename R, typename... Args>
+		struct function_traits_tx<R(C::*)(Args...)>
+			: function_traits_tx<R(Args...)>
+		{
+			constexpr static bool is_memfnptr = true;
+			using class_type = C;
+		};
+
+		// function-type
+		template <typename R, typename... Args>
+		struct function_traits_tx<R(Args...)>
+		{
+			using result_type = R;
+			using tupled_args_type = std::tuple<Args...>;
+
+			template <size_t i>
+			using arg_type = std::tuple_element_t<i, std::tuple<Args...>>;
+
+			constexpr static size_t const arity = sizeof...(Args);
+			constexpr static bool   const is_memfnptr = false;
+			using class_type = void;
 		};
 	}
 
 	template <typename T>
-	constexpr bool const is_callable_v = detail::is_callable_t<T>::value;
-
-
-
-
-
-
-
-	template <typename F>
-	struct function_traits;
-
-
-
-
-	// remove references
-	template <typename F>
-	struct function_traits<F&>
-		: function_traits<F>
+	struct function_traits_override
+		: detail::function_traits_tx<T>
 	{};
 
 	template <typename F>
-	struct function_traits<F&&>
-		: function_traits<F>
-	{};
+	using function_traits = function_traits_override<std::decay_t<F>>;
+	
 
-
-	// presumed "callable"
-	template <typename T>
-	struct function_traits
-		: function_traits<decltype(&T::operator())>
-	{
-		// subtle interaction: a functor is *not* a member-function-pointer,
-		// even though we derive the majority of our definition from the member-
-		// function-pointer of its call-operator
-		static bool const is_memfnptr = false;
-	};
-
-
-	// function-pointer
-	template <typename R, typename... Args>
-	struct function_traits<R(*)(Args...)>
-		: function_traits<R(Args...)>
-	{};
-
-
-	// member-function-pointer
-	template <typename C, typename R, typename... Args>
-	struct function_traits<R(C::*)(Args...) const>
-		: function_traits<R(Args...)>
-	{
-		static bool   const is_memfnptr = true;
-		using class_type = C;
-	};
-
-	template <typename C, typename R, typename... Args>
-	struct function_traits<R(C::*)(Args...)>
-		: function_traits<R(Args...)>
-	{
-		static bool   const is_memfnptr = true;
-		using class_type = C;
-	};
-
-
-	template <typename R, typename... Args>
-	struct function_traits<R(Args...)>
-	{
-		using result_type      = R;
-		using tupled_args_type = std::tuple<Args...>;
-
-		template <size_t i>
-		using arg_type = typename std::tuple_element<i, std::tuple<Args...>>::type;
-
-		constexpr static size_t const arity = sizeof...(Args);
-		constexpr static bool   const is_memfnptr = false;
-		using class_type = void;
-	};
-
-
-
-
-	// reimplement std::result_of
-	template <typename F>
-	struct result_of
-	{
-		using type = std::result_of_t<F>;
-	};
-
-	template <typename F>
-	using result_of_t = typename result_of<F>::type;
 }
