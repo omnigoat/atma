eee386f8ce6e8bd32e4e247ae4460ec14ff308e3
omnigoat <379771+omnigoat@users.noreply.github.com>
2016-05-30 09:38:06 +1000

    refactor streaming operations

--- include/rose/mmap.hpp
+++ include/rose/mmap.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
+#include <rose/rose_fwd.hpp>
+
 #include <atma/config/platform.hpp>
 #include <atma/assert.hpp>
 #include <atma/intrusive_ptr.hpp>
-#include <atma/bitmask.hpp>
 
 #include <filesystem>
 
@@ -12,30 +13,23 @@ namespace stdfs = std::experimental::filesystem;
 
 namespace rose
 {
-	enum class access_flags_t
-	{
-		read,
-		write,
-	};
-
-	ATMA_BITMASK(access_mask_t, access_flags_t);
-
 	struct mmap_t : atma::ref_counted
 	{
 #if ATMA_PLATFORM_WINDOWS
 		using handle_t = HANDLE;
 #endif
 
-		mmap_t(stdfs::path const&, access_mask_t = access_flags_t::read);
+		mmap_t(stdfs::path const&, file_access_mask_t = file_access_t::read);
 		~mmap_t();
 
 		auto valid() const -> bool;
 		auto handle() const -> handle_t;
 		auto size() const -> size_t;
+		auto access_mask() const -> file_access_mask_t;
 
 	private:
 		stdfs::path path_;
-		access_mask_t access_mask_;
+		file_access_mask_t access_mask_;
 
 		handle_t handle_;
 		size_t size_;
@@ -58,6 +52,11 @@ namespace rose
 	inline auto mmap_t::size() const -> size_t
 	{
 		return size_;
+	}
+
+	inline auto mmap_t::access_mask() const -> file_access_mask_t
+	{
+		return access_mask_;
 	}
 }
 
