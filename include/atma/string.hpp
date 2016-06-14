b10fdf4bb531130704db13af35f706a64513ff73
omnigoat <379771+omnigoat@users.noreply.github.com>
2016-06-15 09:31:31 +1000

    string encoding

--- include/atma/string.hpp
+++ include/atma/string.hpp
@@ -16,6 +16,71 @@ namespace atma {
 	using string_range_t = utf8_string_range_t;
 
 
+
+
+
+	struct string_encoder_t
+	{
+		string_encoder_t(char* buf, size_t size);
+		string_encoder_t(string& buf);
+
+		auto operator << (char const*) -> string_encoder_t&;
+		auto operator << (int) -> string_encoder_t&;
+		auto operator << (uint) -> string_encoder_t&;
+
+	private:
+		bool put_buf(char);
+		bool put_str(char);
+
+	private:
+		using put_fn = bool(string_encoder_t::*)(char);
+
+		put_fn put_fn_;
+
+	private:
+		char* buf_ = nullptr;
+		size_t size_ = 0;
+		size_t p_ = 0;
+
+		string* str_ = nullptr;
+	};
+
+	inline string_encoder_t::string_encoder_t(char* buf, size_t size)
+		: buf_(buf), size_(size)
+		, put_fn_(&string_encoder_t::put_buf)
+	{}
+
+	inline bool string_encoder_t::put_buf(char c)
+	{
+		if (p_ == size_)
+			return false;
+
+		buf_[p_++] = c;
+		return true;
+	}
+
+	inline auto string_encoder_t::operator << (char const* str) -> string_encoder_t&
+	{
+		while (*str)
+		{
+			if (!(this->*put_fn_)(*str))
+				break;
+			++str;
+		}
+
+		return *this;
+	}
+
+
+
+
+
+
+
+
+
+
+
 	template <typename T, typename = atma::enable_if<std::is_integral<T>, std::is_unsigned<T>>>
 	inline auto to_string(T x, uint base = 10) -> atma::string
 	{
