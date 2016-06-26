80a951eaaeed5aaf34bc57bad02e92a314a7320b
omni <omnigoat@gmail.com>
2016-06-27 09:07:03 +1000

    bytestream

--- include/atma/streams.hpp
+++ include/atma/streams.hpp
@@ -91,12 +91,12 @@ namespace atma
 // memory-stream
 namespace atma
 {
-	struct memory_stream_t
+	struct memory_bytestream_t
 		: random_access_input_bytestream_t
 		, random_access_output_bytestream_t
 	{
-		memory_stream_t();
-		memory_stream_t(void* data, size_t size_);
+		memory_bytestream_t();
+		memory_bytestream_t(void* data, size_t size_);
 
 		auto valid() const -> bool;
 		auto size() const -> size_t;
@@ -134,34 +134,34 @@ namespace atma
 		size_t size_;
 	};
 
-	inline memory_stream_t::memory_stream_t()
+	inline memory_bytestream_t::memory_bytestream_t()
 		: data_()
 		, size_()
 		, position_()
 	{}
 
-	inline memory_stream_t::memory_stream_t(void* data, size_t size)
+	inline memory_bytestream_t::memory_bytestream_t(void* data, size_t size)
 		: data_(reinterpret_cast<byte*>(data))
 		, size_(size)
 		, position_()
 	{}
 
-	inline auto memory_stream_t::valid() const -> bool
+	inline auto memory_bytestream_t::valid() const -> bool
 	{
 		return data_ != nullptr;
 	}
 
-	inline auto memory_stream_t::size() const -> size_t
+	inline auto memory_bytestream_t::size() const -> size_t
 	{
 		return size_;
 	}
 
-	inline auto memory_stream_t::position() const -> size_t
+	inline auto memory_bytestream_t::position() const -> size_t
 	{
 		return position_;
 	}
 
-	inline auto memory_stream_t::seek(size_t x) -> stream_status_t
+	inline auto memory_bytestream_t::seek(size_t x) -> stream_status_t
 	{
 		if (x < size_) {
 			position_ = x;
@@ -172,7 +172,7 @@ namespace atma
 		}
 	}
 
-	inline auto memory_stream_t::move(int64 x) -> stream_status_t
+	inline auto memory_bytestream_t::move(int64 x) -> stream_status_t
 	{
 		if (position_ + x < size_) {
 			position_ += x;
@@ -183,7 +183,7 @@ namespace atma
 		}
 	}
 
-	inline auto memory_stream_t::read(void* buf, size_t size) -> read_result_t
+	inline auto memory_bytestream_t::read(void* buf, size_t size) -> read_result_t
 	{
 		size_t r = std::min(size, size_ - position_);
 		memcpy(buf, data_ + position_, r);
@@ -194,7 +194,7 @@ namespace atma
 			return{stream_status_t::eof, r};
 	}
 
-	inline auto memory_stream_t::write(void const* data, size_t size) -> write_result_t
+	inline auto memory_bytestream_t::write(void const* data, size_t size) -> write_result_t
 	{
 		size_t r = std::min(size, size_ - position_);
 		memcpy(data_ + position_, data, r);
@@ -207,44 +207,44 @@ namespace atma
 	}
 
 	// absract-stream
-	inline auto memory_stream_t::stream_opers() const -> stream_opers_mask_t
+	inline auto memory_bytestream_t::stream_opers() const -> stream_opers_mask_t
 	{
 		return stream_opers_t::read | stream_opers_t::write | stream_opers_t::random_access;
 	}
 
 	// input-stream
-	inline auto memory_stream_t::g_size() const -> size_t
+	inline auto memory_bytestream_t::g_size() const -> size_t
 	{
 		return size();
 	}
 
-	inline 	auto memory_stream_t::g_seek(size_t x) -> stream_status_t
+	inline 	auto memory_bytestream_t::g_seek(size_t x) -> stream_status_t
 	{
 		return seek(x);
 	}
 
-	inline auto memory_stream_t::g_move(int64 x) -> stream_status_t
+	inline auto memory_bytestream_t::g_move(int64 x) -> stream_status_t
 	{
 		return move(x);
 	}
 
 	// output-stream
-	inline auto memory_stream_t::p_size() const -> size_t
+	inline auto memory_bytestream_t::p_size() const -> size_t
 	{
 		return size();
 	}
 
-	inline auto memory_stream_t::p_seek(size_t x) -> stream_status_t
+	inline auto memory_bytestream_t::p_seek(size_t x) -> stream_status_t
 	{
 		return seek(x);
 	}
 
-	inline auto memory_stream_t::p_move(int64 x) -> stream_status_t
+	inline auto memory_bytestream_t::p_move(int64 x) -> stream_status_t
 	{
 		return move(x);
 	}
 
-	inline auto memory_stream_t::memory_stream_reset(void* data, size_t size) -> void
+	inline auto memory_bytestream_t::memory_stream_reset(void* data, size_t size) -> void
 	{
 		data_ = reinterpret_cast<byte*>(data);
 		size_ = size;
