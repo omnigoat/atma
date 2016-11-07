207cd83a9e6cb69813cb206c19baf0224f6a40a6
omnigoat <379771+omnigoat@users.noreply.github.com>
2016-11-07 18:35:47 +1100

    more work on canary & unit-tests

--- source/atma_test/test_vector.cpp
+++ source/atma_test/test_vector.cpp
@@ -4,233 +4,249 @@
 
 using canary_t = atma::unit_test::canary_t;
 
+
+
 SCENARIO("vectors can be constructed", "[vector]")
 {
 	GIVEN("a default-constructed vector")
 	{
 		atma::vector<int> v;
 
-		CHECK(v.empty());
-		CHECK(v.size() == 0);
-		CHECK(v.capacity() == 0);
-	}
-	
-	GIVEN("a vector constructed with size 4 default items")
-	{
-		CANARY_SWITCH_SCOPE("default-constructed")
+		THEN("the vector is empty")
 		{
-			atma::vector<canary_t> v(4);
+			CHECK(v.empty());
+			CHECK(v.size() == 0);
+			CHECK(v.capacity() == 0);
+		}
+	}
 
+	GIVEN_CANARY("a vector constructed with size of four")
+	{
+		atma::vector<canary_t> v(4);
+
+		THEN("I have four default-constructed elements")
+		{
 			CHECK(!v.empty());
 			CHECK(v.size() == 4);
 			CHECK(v.capacity() >= 4);
 		}
-
-		CHECK_CANARY_SCOPE("default-constructed",
-			// default-construct in v
-			{1, canary_t::default_constructor},
-			{2, canary_t::default_constructor},
-			{3, canary_t::default_constructor},
-			{4, canary_t::default_constructor},
-			// v destructs
-			{1, canary_t::destructor},
-			{2, canary_t::destructor},
-			{3, canary_t::destructor},
-			{4, canary_t::destructor},
-		);
 	}
-
-	GIVEN("a vector constructed with size 4 copy-constructed items")
-	{	
-		CANARY_SWITCH_SCOPE("copy-constructed")
-		{
-			atma::vector<canary_t> v(4, canary_t{13});
-
-			CHECK(!v.empty());
-			CHECK(v.size() == 4);
-			CHECK(v.capacity() >= 4);
-			CHECK(v[0].payload == 13); CHECK(v[1].payload == 13); CHECK(v[2].payload == 13); CHECK(v[3].payload == 13);
-		}
-
-		CHECK_CANARY_SCOPE("copy-constructed",
-			// temporary constructs
-			{1, canary_t::direct_constructor},
-			// four copies
-			{2, canary_t::copy_constructor},
-			{3, canary_t::copy_constructor},
-			{4, canary_t::copy_constructor},
-			{5, canary_t::copy_constructor},
-			// temporary destructs
-			{1, canary_t::destructor},
-			// v destructs
-			{2, canary_t::destructor},
-			{3, canary_t::destructor},
-			{4, canary_t::destructor},
-			{5, canary_t::destructor},
-		);
-	}
-
-	GIVEN("a vector constructed with {1, 2, 3, 4}")
+	THEN_CANARY
 	{
-		atma::vector<int> v{1, 2, 3, 4};
-
-		CANARY_SWITCH_SCOPE("initializer-list")
-		{
-			atma::vector<canary_t> v{1, 2, 3, 4};
-
-			CHECK(!v.empty());
-			CHECK(v.size() == 4);
-			CHECK(v.capacity() >= 4);
-			CHECK(v[0] == 1); CHECK(v[1] == 2); CHECK(v[2] == 3); CHECK(v[3] == 4);
-		}
-
-		CHECK_CANARY_SCOPE("initializer-list",
-
-			// initializer-list
-			CANARY_CC_ORDER(
-				{1, canary_t::direct_constructor},
-				{2, canary_t::direct_constructor},
-				{3, canary_t::direct_constructor},
-				{4, canary_t::direct_constructor}),
-
-			// copy-construct into v
-			{5, canary_t::copy_constructor},
-			{6, canary_t::copy_constructor},
-			{7, canary_t::copy_constructor},
-			{8, canary_t::copy_constructor},
-
-			// initializer-list destructs
-			CANARY_CC_ORDER(
-				{4, canary_t::destructor},
-				{3, canary_t::destructor},
-				{2, canary_t::destructor},
-				{1, canary_t::destructor}),
-
-			// v destructs
-			{5, canary_t::destructor},
-			{6, canary_t::destructor},
-			{7, canary_t::destructor},
-			{8, canary_t::destructor},
-		);
-
-		THEN("another number can be inserter at the front")
-		{
-			v.insert(v.begin(), 0);
-
-			CHECK(v.size() == 5);
-			CHECK(v[0] == 0); CHECK(v[1] == 1); CHECK(v[2] == 2); CHECK(v[3] == 3); CHECK(v[4] == 4);
-		}
+		// default-construct in v
+		C.default_constructor(1);
+		C.default_constructor(2);
+		C.default_constructor(3);
+		C.default_constructor(4);
+		// v destructs
+		C.destructor(1);
+		C.destructor(2);
+		C.destructor(3);
+		C.destructor(4);
 	}
 
-	GIVEN("a copy-constructed vector")
+	GIVEN_CANARY("a vector constructed with size 4 copy-constructed items")
 	{
-		CANARY_SWITCH_SCOPE("copy-construct-vector")
-		{
-			atma::vector<canary_t> v1{1, 2, 3, 4};
-			atma::vector<canary_t> v2{v1};
+		atma::vector<canary_t> v(4, canary_t{13});
 
+		THEN("there are four duplicates")
+		{
+			CHECK(!v.empty());
+			CHECK(v.capacity() >= 4);
+			CHECK_WHOLE_VECTOR(v, 13, 13, 13, 13);
+		}
+	}
+	THEN_CANARY
+	{
+		// temporary constructs
+		C.direct_constructor(1);
+		// four copies
+		C.copy_constructor(2);
+		C.copy_constructor(3);
+		C.copy_constructor(4);
+		C.copy_constructor(5);
+		// temporary destructs
+		C.destructor(1);
+		// v destructs
+		C.destructor(2);
+		C.destructor(3);
+		C.destructor(4);
+		C.destructor(5);
+	}
+
+	GIVEN_CANARY("a vector constructed with {1, 2, 3, 4}")
+	{
+		atma::vector<canary_t> v{1, 2, 3, 4};
+		
+		THEN("we have those elements present")
+		{
+			CHECK(!v.empty());
+			CHECK(v.capacity() >= 4);
+			CHECK_WHOLE_VECTOR(v, 1, 2, 3, 4);
+		}
+	}
+	THEN_CANARY
+	{
+		// direct-construct initializer-list
+		C.direct_constructor(1);
+		C.direct_constructor(2);
+		C.direct_constructor(3);
+		C.direct_constructor(4);
+		// copy-construct into v
+		C.copy_constructor(5);
+		C.copy_constructor(6);
+		C.copy_constructor(7);
+		C.copy_constructor(8);
+		// initializer-list destructs
+		C.destructor(4);
+		C.destructor(3);
+		C.destructor(2);
+		C.destructor(1);
+		// v destructs
+		C.destructor(5);
+		C.destructor(6);
+		C.destructor(7);
+		C.destructor(8);
+	}
+
+
+
+	GIVEN_CANARY("a vector copy-constructed from {1, 2, 3, 4}")
+	{
+		atma::vector<canary_t> v1{1, 2, 3, 4};
+		atma::vector<canary_t> v2{v1};
+
+		THEN("we have equivalent {1,2,3,4} & {1,2,3,4}")
+		{
 			CHECK(!v2.empty());
-			CHECK(v2.size() == 4);
 			CHECK(v2.capacity() >= 4);
-			CHECK(v2[0].payload == 1); CHECK(v2[1].payload == 2); CHECK(v2[2].payload == 3); CHECK(v2[3].payload == 4);
+			CHECK_WHOLE_VECTOR(v2, 1,2,3,4);
 			CHECK(v2 == v1);
 		}
-
-		CHECK_CANARY_SCOPE("copy-construct-vector",
-
-			// construction of temporaries
-			CANARY_CC_ORDER(
-				{1, canary_t::direct_constructor},
-				{2, canary_t::direct_constructor},
-				{3, canary_t::direct_constructor},
-				{4, canary_t::direct_constructor}),
-
-			// copy-construct into v1
-			{5, canary_t::copy_constructor},
-			{6, canary_t::copy_constructor},
-			{7, canary_t::copy_constructor},
-			{8, canary_t::copy_constructor},
-
-			// destruct temporaries
-			CANARY_CC_ORDER(
-				{4, canary_t::destructor},
-				{3, canary_t::destructor},
-				{2, canary_t::destructor},
-				{1, canary_t::destructor}),
-
-			// copy-construct into v2
-			{9, canary_t::copy_constructor},
-			{10, canary_t::copy_constructor},
-			{11, canary_t::copy_constructor},
-			{12, canary_t::copy_constructor},
-
-			// v2 destructs
-			{9, canary_t::destructor},
-			{10, canary_t::destructor},
-			{11, canary_t::destructor},
-			{12, canary_t::destructor},
-
-			// v1 destructs
-			{5, canary_t::destructor},
-			{6, canary_t::destructor},
-			{7, canary_t::destructor},
-			{8, canary_t::destructor},
-		);
+	}
+	THEN_CANARY
+	{
+		// construction of temporaries
+		C.direct_constructor(1);
+		C.direct_constructor(2);
+		C.direct_constructor(3);
+		C.direct_constructor(4);
+		// copy-construct into v1 (not the title case)
+		C.copy_constructor(5);
+		C.copy_constructor(6);
+		C.copy_constructor(7);
+		C.copy_constructor(8);
+		// destruct temporaries
+		C.destructor(4);
+		C.destructor(3);
+		C.destructor(2);
+		C.destructor(1);
+		// copy-construct into v2
+		C.copy_constructor(9);
+		C.copy_constructor(10);
+		C.copy_constructor(11);
+		C.copy_constructor(12);
+		// v2 destructs
+		C.destructor(9);
+		C.destructor(10);
+		C.destructor(11);
+		C.destructor(12);
+		// v1 destructs
+		C.destructor(5);
+		C.destructor(6);
+		C.destructor(7);
+		C.destructor(8);
 	}
 
-	GIVEN("a move-constructed vector")
-	{
-		CANARY_SWITCH_SCOPE("vector::move-constructor")
-		{
-			atma::vector<canary_t> v1{1, 2, 3, 4};
-			atma::vector<canary_t> v2{std::move(v1)};
 
-			// original vector empty
+
+	GIVEN_CANARY("a move-constructed vector")
+	{
+		atma::vector<canary_t> v1{1, 2, 3, 4};
+		atma::vector<canary_t> v2{std::move(v1)};
+
+		THEN("original vector is empty")
+		{
 			CHECK(v1.empty());
 			CHECK(v1.size() == 0);
 			CHECK(v1.capacity() == 0);
-
-			// new vector initialized correctly
-			CHECK(!v2.empty());
-			CHECK(v2.size() == 4);
-			CHECK(v2.capacity() >= 4);
-			CHECK(v2[0] == 1); CHECK(v2[1] == 2); CHECK(v2[2] == 3); CHECK(v2[3] == 4);
 		}
 
-		CHECK_CANARY_SCOPE("vector::move-constructor",
+		THEN("new vector initialized correctly")
+		{
+			CHECK(!v2.empty());
+			CHECK(v2.capacity() >= 4);
+			CHECK_WHOLE_VECTOR(v2, 1,2,3,4);
+		}
+	}
+	THEN_CANARY
+	{
+		// construction of temporaries
+		C.direct_constructor(1);
+		C.direct_constructor(2);
+		C.direct_constructor(3);
+		C.direct_constructor(4);
+		// temporaries copy-constructed into v1
+		C.copy_constructor(5);
+		C.copy_constructor(6);
+		C.copy_constructor(7);
+		C.copy_constructor(8);
+		// destruct temporaries
+		C.destructor(4);
+		C.destructor(3);
+		C.destructor(2);
+		C.destructor(1);
+		// move-constructor of vector just swaps buffers, nothing happens
+		//
+		// v1 destructs, now empty
+		//
+		// v2 destructs
+		C.destructor(5);
+		C.destructor(6);
+		C.destructor(7);
+		C.destructor(8);
+	}
 
-			// construction of temporaries
-			CANARY_CC_ORDER(
-				{1, canary_t::direct_constructor},
-				{2, canary_t::direct_constructor},
-				{3, canary_t::direct_constructor},
-				{4, canary_t::direct_constructor}),
+}
 
-			// copy-construct into v1
-			{5, canary_t::copy_constructor},
-			{6, canary_t::copy_constructor},
-			{7, canary_t::copy_constructor},
-			{8, canary_t::copy_constructor},
+SCENARIO("vectors can be inserted into", "[vector]")
+{
+	GIVEN("a vector constructed with {1, 2, 3, 4}")
+	{
+		atma::vector<int> n{1, 2, 3, 4};
 
-			// destruct temporaries
-			CANARY_CC_ORDER(
-				{4, canary_t::destructor},
-				{3, canary_t::destructor},
-				{2, canary_t::destructor},
-				{1, canary_t::destructor}),
+		WHEN("number 0 is inserted at the front")
+		{
+			n.insert(n.begin(), 0);
 
-			// move-constructor of vector just swaps buffers, nothing happens
+			THEN("{0, 1, 2, 3, 4}")
+			{
+				CHECK_WHOLE_VECTOR(n, 0, 1, 2, 3, 4);
+			}
+		}
 
-			// v1 destructs, now empty
+		WHEN("number 5 is inserted at the back")
+		{
+			n.insert(n.end(), 5);
 
-			// v2 destructs
-			{5, canary_t::destructor},
-			{6, canary_t::destructor},
-			{7, canary_t::destructor},
-			{8, canary_t::destructor},
-		);
+			THEN("{1, 2, 3, 4, 5}")
+			{
+				CHECK_WHOLE_VECTOR(n, 1, 2, 3, 4, 5);
+			}
+		}
+
+		WHEN("number 17 is inserter at index 2")
+		{
+			n.insert(n.begin() + 2, 17);
+
+			THEN("looks like: {1, 2, 17, 3, 4}")
+			{
+				CHECK_WHOLE_VECTOR(n, 1, 2, 17, 3, 4);
+			}
+		}
 	}
 }
+
 
 SCENARIO("vectors can be sized and resized", "[vector]")
 {
@@ -282,16 +298,16 @@ SCENARIO("vectors can be sized and resized", "[vector]")
 
 SCENARIO("vectors can be assigned", "[vector]")
 {
-	GIVEN("an empty vector and vector of four components")
+	GIVEN("an empty vector 'v' and vector v2{1,2,3,4}")
 	{
 		atma::vector<int> v;
 		atma::vector<int> v2{1, 2, 3, 4};
 
-		WHEN("v is assigned v2")
+		WHEN("the {1,2,3,4} vector is assigned to the empty-vector")
 		{
 			v = v2;
 
-			THEN("a copy is made")
+			THEN("an exact copy is made")
 			{
 				CHECK(!v.empty());
 				CHECK(v.size() == 4);
@@ -303,15 +319,17 @@ SCENARIO("vectors can be assigned", "[vector]")
 		{
 			v = std::move(v2);
 
-			THEN("v2 is reduced to nothing")
+			THEN("v has the elements")
 			{
 				CHECK(!v.empty());
 				CHECK(v.size() == 4);
+				CHECK_WHOLE_VECTOR(v, 1, 2, 3, 4);
+			}
+
+			THEN("v2 is reduced to nothing")
+			{
 				CHECK(v2.empty());
 				CHECK(v2.capacity() == 0);
-
-				atma::vector<int> t{1, 2, 3, 4};
-				CHECK(v == t);
 			}
 		}
 	}
