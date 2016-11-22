3ceb95f6d98201ec02f8d94ec9dc58cb35e15de0
omnigoat <379771+omnigoat@users.noreply.github.com>
2016-11-22 18:24:50 +1100

    updated traits
     - added is_function_reference
     - improved implementation of is_function_pointer
     - refactored SFINAE logic into macro ATMA_SFINAE_TEST_METHOD
     - reimplemented has_functor_operator to use macro

--- include/atma/types.hpp
+++ include/atma/types.hpp
@@ -30,6 +30,21 @@ typedef uintptr_t uintptr;
 #define ATMA_SPLAT_FN_II(counter, pattern) int ATMA_PP_CAT(splat, counter)[] = {0, (pattern, void(), 0)...};
 #define ATMA_SPLAT_FN(pattern) ATMA_SPLAT_FN_II(__COUNTER__, pattern)
 
+#define ATMA_SFINAE_TEST_METHOD(name, oper) \
+	template <typename T> \
+	struct name##_t \
+	{ \
+	private: \
+		template <typename C> constexpr static bool test(decltype(&C::oper)) { return true; } \
+		template <typename C> constexpr static bool test(...) { return false; } \
+		\
+	public: \
+		static constexpr bool value = test<T>(0); \
+	}; \
+	template <typename T> constexpr bool name##_v = name##_t<T>::value
+
+
+
 namespace atma
 {
 	//
@@ -40,15 +55,56 @@ namespace atma
 	template <typename T, typename M>
 	using transfer_const_t = std::conditional_t<std::is_const<T>::value, M const, M>;
 
+
 	//
-	//  not
-	//  -----
-	//    negates a predicate type
+	//  has_functor_operator
+	//  ----------------------
+	//    boolean. true if type T has an `operator ()`
+	//
+	ATMA_SFINAE_TEST_METHOD(has_functor_operator, operator ());
+
+
+	//
+	//  is_function_pointer
+	//  ---------------------
+	//    srsly, c++ std.
 	//
 	template <typename T>
-	struct not_v
-	{
-		static auto const value = !T::value;
-		using type = bool;
-	};
+	struct is_function_pointer_t : std::conditional_t<
+		std::is_pointer_v<T>,
+		std::is_function<std::remove_pointer_t<T>>,
+		std::false_type>
+	{};
+
+	template <typename T>
+	constexpr static bool is_function_pointer_v = is_function_pointer_t<T>::value;
+
+
+	//
+	//  is_function_reference
+	//  -----------------------
+	//    srsly, c++ std.
+	//
+	template <typename T>
+	struct is_function_reference_t : std::conditional_t<
+		std::is_reference_v<T>,
+		std::is_function<std::remove_reference_t<T>>,
+		std::false_type>
+	{};
+
+	template <typename T>
+	constexpr static bool is_function_reference_v = is_function_reference_t<T>::value;
+
+
+	//
+	//  is_callable_v
+	//  ---------------
+	//    returns true if T can be used like "T(args...)"
+	//
+	template <typename T>
+	constexpr bool is_callable_v =
+		is_function_pointer_v<T> ||
+		is_function_reference_v<T> ||
+		std::is_member_function_pointer_v<T> ||
+		has_functor_operator_v<T>;
 }
