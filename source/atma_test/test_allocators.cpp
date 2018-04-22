ac9633516833f38e82f9885bdce81071e1e81c2a
omnigoat <omnigoat@gmail.com>
2018-04-23 04:23:21 +1000

    more updates

--- source/atma_test/test_allocators.cpp
+++ source/atma_test/test_allocators.cpp
@@ -8,8 +8,10 @@ SCENARIO("arena allocator can be constructed", "[allocators]")
 {
 	GIVEN("a default-constructed arena-allocator")
 	{
-		atma::arena_allocator_t A{4096, 64, std::pmr::get_default_resource()};
-		void* memory = A.allocate(20);
-		void* memory2 = A.allocate(20);
+		atma::arena_allocator_t A{64, 8, std::pmr::get_default_resource()};
+		void* M1 = A.allocate(20);
+		void* M2 = A.allocate(20);
+		void* M3 = A.allocate(20);
+		void* M4 = A.allocate(20);
 	}
 }
