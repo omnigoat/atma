21bd2a3a3ca82d7813b1d44546732f45616b479a
omnigoat <379771+omnigoat@users.noreply.github.com>
2018-05-27 18:21:03 +1000

    refactored flyweight

--- include/atma/flyweight.hpp
+++ include/atma/flyweight.hpp
@@ -2,35 +2,46 @@
 
 #include <atma/intrusive_ptr.hpp>
 
+#include <memory>
+
 
 namespace atma
 {
 	template <typename T>
 	struct flyweight_t
 	{
-		flyweight_t()
-			: backend_{new T}
-		{}
+		using backend_ptr_t = std::conditional_t<std::is_convertible_v<T*, ref_counted*>, intrusive_ptr<T>, std::shared_ptr<T>>;
 
-		template <typename... Args>
-		flyweight_t(Args&&... args)
-			: backend_{new T{std::forward<Args>(args)...}}
-		{}
-
+		flyweight_t();
 		flyweight_t(flyweight_t const&) = default;
 		flyweight_t(flyweight_t&&) = default;
+
+		template <typename... Args>
+		flyweight_t(Args&&...);
 
 		auto operator = (flyweight_t const&) -> flyweight_t& = default;
 		auto operator = (flyweight_t&&) -> flyweight_t& = default;
 
 	protected:
-		auto backend() const -> T& { return *backend_; }
-		auto weak_backend() const -> std::weak_ptr<T> { return backend_; }
+		decltype(auto) backend() const { return *backend_; }
+		decltype(auto) backend_ptr() const { return backend_; }
 
 	private:
-		using storage_ptr_t = std::conditional_t<std::is_convertible_v<T*, ref_counted*>, intrusive_ptr<T>, std::shared_ptr<T>>;
-
-		storage_ptr_t backend_;
+		backend_ptr_t backend_;
 	};
+}
+
+namespace atma
+{
+	template <typename T>
+	inline flyweight_t<T>::flyweight_t()
+		: backend_{new T}
+	{}
+
+	template <typename T>
+	template <typename... Args>
+	inline flyweight_t<T>::flyweight_t(Args&&... args)
+		: backend_{new T{std::forward<Args>(args)...}}
+	{}
 }
 
