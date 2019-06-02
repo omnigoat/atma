72ba11f19161ba5311d2bef6781c22fc748ce629
omnigoat <379771+omnigoat@users.noreply.github.com>
2019-06-03 02:21:46 +1000

    some tests for memory before refactor 40

--- source/atma_test/memory.cpp
+++ source/atma_test/memory.cpp
@@ -3,18 +3,65 @@
 #include <atma/memory.hpp>
 
 
-SCENARIO("ranges can be filtered", "[memory/base_memory_t]")
+SCENARIO("base_memory_t can EBO properly", "[memory/base_memory_t]")
 {
 	GIVEN("an empty allocator")
 	{
 		using empty_allocator = atma::aligned_allocator_t<byte>;
+		static_assert(std::is_empty_v<empty_allocator>, "empty_allocator must actually be empty");
 
 		THEN("sizeof memory_t isn't more than a pointer")
 		{
-			atma::detail::base_memory_t<byte, empty_allocator> blam;
-			static_assert(std::is_empty_v<empty_allocator>, "empty_allocator must actually be empty");
+			atma::detail::base_memory_t<byte, empty_allocator> memory;
+			CHECK(sizeof(memory) == sizeof(byte*));
+		}
+	}
+}
 
-			CHECK(sizeof(blam) == sizeof(byte*));
+
+SCENARIO("simple_memory_t behaves nicely", "[memory/simple_memory_t]")
+{
+	GIVEN("an empty allocator")
+	{
+		using empty_allocator = atma::aligned_allocator_t<byte>;
+		using memory_t = atma::simple_memory_t<byte, empty_allocator>;
+
+		THEN("simple_memory_t can be default-constructed")
+		{
+			auto memory = memory_t();
+		}
+
+		THEN("simple_memory_t can constructed from a pointer & allocator")
+		{
+			byte* ptr = nullptr;
+			auto memory = memory_t(ptr);
+		}
+	}
+}
+
+
+SCENARIO("basic_memory_t behaves nicely", "[memory/basic_memory_t]")
+{
+	GIVEN("an empty allocator")
+	{
+		using empty_allocator = atma::aligned_allocator_t<byte>;
+		using memory_t = atma::basic_memory_t<byte, empty_allocator>;
+
+		THEN("basic_memory_t can be default-constructed")
+		{
+			auto memory = memory_t();
+		}
+
+		THEN("basic_memory_t can constructed from a pointer")
+		{
+			byte* ptr = nullptr;
+			auto memory = memory_t(ptr);
+		}
+
+		THEN("basic_memory_t can constructed from a pointer")
+		{
+			byte* ptr = nullptr;
+			auto memory = memory_t(ptr);
 		}
 	}
 }
