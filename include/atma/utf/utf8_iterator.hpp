a70729c42d4addef11fe652b549216c71072c97a
omnigoat <omnigoat@gmail.com>
2020-02-23 16:31:15 +1100

    change byte to refer to std::byte, fix previous usage

--- include/atma/utf/utf8_iterator.hpp
+++ include/atma/utf/utf8_iterator.hpp
@@ -1,37 +1,5 @@
-#ifndef ATMA_UTF_UTF8_ITERATOR
-#define ATMA_UTF_UTF8_ITERATOR
-//=====================================================================
+#pragma once
+
 #include <atma/assert.hpp>
 #include <atma/utf/algorithm.hpp>
-//=====================================================================
-namespace atma {
-//=====================================================================
-	
-	struct utf8_iterator
-	{
-		utf8_iterator(char* begin);
 
-		auto operator *() const -> char;
-		auto operator ++() -> utf8_iterator&;
-		auto operator --() -> utf8_iterator&;
-		
-	private:
-		char* here_;
-	};
-
-	auto operator utf8_iterator::operator++ () -> utf8_iterator&
-	{
-		here_ = utf8_next_char(here_);
-		return *this;
-	}
-
-	auto operator utf8_iterator::operator *() -> char
-	{
-		return *here_;
-	}
-
-//=====================================================================
-} // namespace atma
-//=====================================================================
-#endif // inclusion guard
-//=====================================================================
