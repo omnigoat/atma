ea8b7756a75c8efc1e8ae341e272a98e0f6f2543
omnigoat <omnigoat@gmail.com>
2020-08-22 11:39:34 +1000

    add more utf8 tests

--- source/atma_test/test_utf8_string.cpp
+++ source/atma_test/test_utf8_string.cpp
@@ -3,6 +3,66 @@
 #include <atma/utf/utf8_string.hpp>
 
 
+SCENARIO_TEMPLATE("utf8_span_t is used by user", span_type, atma::utf8_span_t, atma::utf8_const_span_t)
+{
+	WHEN("utf8_span is default-constructed")
+	{
+		span_type span;
+
+		THEN("it points to nullptr")
+		AND_THEN("its size is zero")
+		AND_THEN(".empty() evaluates to true")
+		{
+			CHECK(span.data() == nullptr);
+			CHECK(span.size() == 0);
+			CHECK(span.empty());
+		}
+	}
+
+	WHEN("utf8_span is copy-constructed")
+	{
+		atma::utf8_string_t test{"hello good sir"};
+
+		GIVEN("an originating span constructed around the range 'hello good sir'")
+		AND_GIVEN("a span copy-constructed from our originating span")
+		{
+			span_type span1{test};
+			span_type span2{span1};
+
+			THEN("the copy-constructed span equates its originating span")
+			{
+				CHECK(span2.data() == span1.data());
+				CHECK(span2.size() == span1.size());
+			}
+		}
+	}
+
+	WHEN("utf8_span is direct-constructed from a range")
+	{
+		atma::utf8_string_t test{"hello good sir"};
+
+		//static_assert(std::movable<atma::utf8_string_t::iterator>);
+		//static_assert(std::movable<atma::utf8_string_t::iterator>);
+		
+		static_assert(std::input_or_output_iterator<atma::utf8_string_t::iterator>);
+		static_assert(std::ranges::forward_range<atma::utf8_string_t>, "not a range");
+		//std::ranges::begin(test);
+		//std::ranges::end(test);
+
+		GIVEN("a utf8_string_t of 'hello good sir'")
+		AND_GIVEN("a utf8_span directly-constructed from said string")
+		{
+			span_type span{test};
+
+			THEN("the directly constructed span covers the length of the string")
+			{
+				CHECK(span.data() == test.data());
+				CHECK(span.size_bytes() == test.size_bytes());
+			}
+		}
+	}
+}
+
 SCENARIO_OF("utf8_string_t", "utf8-strings can be constructed")
 {
 	GIVEN("a default-constructed string")
@@ -27,13 +87,13 @@ SCENARIO_OF("utf8_string_t", "utf8-strings can be constructed")
 
 		THEN("they compare to \"dragon\"")
 		{
-			CHECK(s1.raw_size() == 6);
+			CHECK(s1.size_bytes() == 6);
 			CHECK(s1 == "dragon");
 
-			CHECK(s2.raw_size() == 6);
+			CHECK(s2.size_bytes() == 6);
 			CHECK(s2 == "dragon");
 
-			CHECK(s2.raw_size() == 6);
+			CHECK(s2.size_bytes() == 6);
 			CHECK(s3 == "dragon");
 		}
 
@@ -54,7 +114,7 @@ SCENARIO_OF("utf8_string_t", "utf8-strings can be constructed")
 
 		THEN("they don't equate to the common erroneous encoding {├┤, µô┐, Úèî, Ù«¿}")
 		{
-			CHECK(s1.raw_size() == 17);
+			CHECK(s1.size_bytes() == 17);
 			CHECK(s1 != s2);
 		}
 	}
