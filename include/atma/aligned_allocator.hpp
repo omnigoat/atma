74ef521ea46a3521d4270af197637f9719eb4de9
omnigoat <379771+omnigoat@users.noreply.github.com>
2020-11-28 16:20:39 +1100

    remove void specialization of aligned_memory

--- include/atma/aligned_allocator.hpp
+++ include/atma/aligned_allocator.hpp
@@ -19,55 +19,40 @@ namespace atma
 		template <typename U>
 		struct rebind { using other = aligned_allocator_t<U, A>; };
 
-		aligned_allocator_t() noexcept {}
+		aligned_allocator_t() noexcept
+		{}
 
 		template <typename U>
-		aligned_allocator_t(aligned_allocator_t<U, A> const&) noexcept {}
+		aligned_allocator_t(aligned_allocator_t<U, A> const&) noexcept
+		{}
 
-		auto allocate(size_type) -> pointer;
-		auto deallocate(pointer, size_type) -> void;
-	};
-
-	template <size_t A>
-	struct aligned_allocator_t<void, A>
-	{
-		using value_type = void;
-		using pointer = value_type*;
-		using const_pointer = value_type const*;
-
-		template <typename U>
-		struct rebind { using other = aligned_allocator_t<U, A>; };
-	};
-
-
-	template <typename T, size_t A>
-	inline auto aligned_allocator_t<T, A>::allocate(size_type n) -> pointer
-	{
-		void* ptr = platform::allocate_aligned_memory(A, n * sizeof(T));
-		if (ptr == nullptr)
+		auto allocate(size_type n) -> pointer
 		{
-			throw std::bad_alloc();
+			void* ptr = platform::allocate_aligned_memory(A, n * sizeof(T));
+			if (ptr == nullptr)
+			{
+				throw std::bad_alloc();
+			}
+
+			return reinterpret_cast<pointer>(ptr);
 		}
 
-		return reinterpret_cast<pointer>(ptr);
+		auto deallocate(pointer p, size_type) -> void
+		{
+			platform::deallocate_aligned_memory(p);
+		}
+	};
+
+
+	template <typename T, size_t TA>
+	inline bool operator == (aligned_allocator_t<T, TA> const&, aligned_allocator_t<T, TA> const&)
+	{
+		return true;
 	}
 
-	template <typename T, size_t A>
-	inline auto aligned_allocator_t<T, A>::deallocate(pointer p, size_type) -> void
+	template <typename T, size_t TA>
+	inline bool operator != (aligned_allocator_t<T, TA> const&, aligned_allocator_t<T, TA> const&)
 	{
-		platform::deallocate_aligned_memory(p);
-	}
-
-
-	template <typename T, size_t TA, typename U, size_t UA>
-	inline bool operator == (aligned_allocator_t<T, TA> const&, aligned_allocator_t<U, UA> const&)
-	{
-		return TA == UA;
-	}
-
-	template <typename T, size_t TA, typename U, size_t UA>
-	inline bool operator != (aligned_allocator_t<T, TA> const&, aligned_allocator_t<U, UA> const&)
-	{
-		return TA != UA;
+		return false;
 	}
 }
