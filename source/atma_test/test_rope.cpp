b692588929eeddfbb83ed3cfef124c3fe4049373
omnigoat <379771+omnigoat@users.noreply.github.com>
2021-09-24 11:41:48 +1000

    begin work on btree algorithms

--- source/atma_test/test_rope.cpp
+++ source/atma_test/test_rope.cpp
@@ -20,7 +20,20 @@ using test_rope_t = atma::basic_rope_t<atma::rope_test_traits>;
 
 SCENARIO("rope can be constructed" * doctest::skip())
 {
+	using T = atma::rope_test_traits;
 	test_rope_t rope;
+
+	auto internal_node = atma::_rope_::make_internal_ptr<T>();
+	atma::_rope_::node_info_t<T> internal_info{internal_node};
+
+	auto A = atma::_rope_::make_leaf_ptr<T>(atma::xfer_src("A", 1));
+	atma::_rope_::node_info_t<T> A_info{A};
+
+	auto B = atma::_rope_::make_leaf_ptr<T>(atma::xfer_src("B", 1));
+	atma::_rope_::node_info_t<T> B_info{B};
+
+	auto postAresult = atma::_rope_::insert_<T>(internal_node, 0, A_info);
+	auto postBresult = atma::_rope_::replace_<T>(internal_node, 0, B_info);
 }
 
 SCENARIO("rope can be inserted" * doctest::skip())
