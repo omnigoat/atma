2d2402dbb8890c9586c8f1301c1d413f23cfb3bc
omnigoat <379771+omnigoat@users.noreply.github.com>
2021-11-03 11:25:07 +1100

    refactor idxs to be a thin-wrapper for std::index_sequence

--- include/atma/idxs.hpp
+++ include/atma/idxs.hpp
@@ -1,60 +1,56 @@
 #pragma once
 
-namespace atma {
+#include <utility>
 
-	namespace detail
+
+//
+//  idxs_t
+//  --------
+//
+namespace atma
+{
+	template <size_t... Idxs>
+	using idxs_t = std::index_sequence<Idxs...>;
+}
+
+
+//
+//  idxs_list_t
+//  idxs_range_t
+//  --------------
+//    creates an idxs_t<> of [begin, end), interpolated by step
+//
+//      idxs_list_t<4> === idxs_t<0, 1, 2, 3>
+//      idxs_range_t<5, 9> === idxs_t<5, 6, 7, 8>
+//      idxs_range_t<7, 3, -1> === idxs_t<7, 6, 5, 4>
+//      idxs_range_t<7, 3, -2> === idxs_t<7, 5>
+//      idxs_range_t<7, 3, -3> === such compilation fail
+//
+namespace atma::detail
+{
+	template <size_t Begin, size_t End, int Step, size_t... Idxs>
+	struct idxs_range_t_
 	{
-		template <int, int, int, int...> struct idxs_range_tx;
-	}
+		static_assert(Step != 0, "bad arguments to idxs_range_tx");
+		static_assert(Step > 0 || Begin > End, "bad arguments to idxs_range_tx");
+		static_assert(Step < 0 || Begin < End, "bad arguments to idxs_range_tx");
 
-
-	//
-	//  idxs_t
-	//  --------
-	//    a structure that stores a variadic list of ints
-	//
-	template <int... idxs>
-	struct idxs_t
-	{
+		using type = typename idxs_range_t_<size_t(Begin + Step), End, Step, Idxs..., Begin>::type;
 	};
-
-
-
-
-	//
-	//  idxs_list_t
-	//  idxs_range_t
-	//  --------------
-	//    creates an idxs_t<> of [@begin, @end)
-	//
-	//      idxs_list_t<4> === idxs_t<0, 1, 2, 3>
-	//      idxs_range_t<5, 9> === idxs_t<5, 6, 7, 8>
-	//      idxs_range_t<7, 3, -1> === idxs_t<7, 6, 5, 4>
-	//      idxs_range_t<7, 3, -3> === such compilation fail
-	//
-	namespace detail
+	
+	template <size_t Terminal, int Step, size_t... Idxs>
+	struct idxs_range_t_<Terminal, Terminal, Step, Idxs...>
 	{
-		template <int begin, int end, int step, int... idxs>
-		struct idxs_range_tx
-		{
-			static_assert(step != 0, "bad arguments to idxs_range_tx");
-			static_assert(step > 0 || begin > end, "bad arguments to idxs_range_tx");
-			static_assert(step < 0 || begin < end, "bad arguments to idxs_range_tx");
+		using type = idxs_t<Idxs...>;
+	};
+}
 
-			using type = typename idxs_range_tx<begin + step, end, step, idxs..., begin>::type;
-		};
+namespace atma
+{
+	// forward to std::make_index_sequence for compilation performance
+	template <size_t Count>
+	using idxs_list_t = std::make_index_sequence<Count>;
 
-		template <int t, int step, int... idxs>
-		struct idxs_range_tx<t, t, step, idxs...>
-		{
-			using type = idxs_t<idxs...>;
-		};
-	}
-
-	template <int count>
-	using idxs_list_t = typename detail::idxs_range_tx<0, count, 1>::type;
-
-	template <int begin, int end, int step = 1>
-	using idxs_range_t = typename detail::idxs_range_tx<begin, end, step>::type;
-
+	template <size_t Begin, size_t End, int Step = 1>
+	using idxs_range_t = typename detail::idxs_range_t_<Begin, End, Step>::type;
 }
