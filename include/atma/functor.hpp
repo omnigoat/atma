cff710af821d6d631788dcac29872ab85c8f6c2a
omnigoat <379771+omnigoat@users.noreply.github.com>
2024-10-08 12:33:54 +1100

    finalise name for functor_cascade, simplify implementation

--- include/atma/functor.hpp
+++ include/atma/functor.hpp
@@ -6,17 +6,17 @@
 namespace atma
 {
 	template <typename... Fwds>
-	struct functor_list_fwds_t
+	struct functor_cascade_fwds_t
 	{};
 }
 
-namespace atma::detail::_functor_list_
+namespace atma::detail::_functor_cascade_
 {
 	template <typename Fwds, typename Gs, typename F>
-	struct functor_delegate_t;
+	struct dispatcher_t;
 
 	template <typename... Fwds, typename... Gs, typename F>
-	struct functor_delegate_t<functor_list_fwds_t<Fwds...>, std::tuple<Gs...>, F>
+	struct dispatcher_t<functor_cascade_fwds_t<Fwds...>, std::tuple<Gs...>, F>
 	{
 		template <typename... Args>
 		requires (!std::is_invocable_v<Gs, Fwds..., Args...> && ...) && std::is_invocable_v<F, Fwds..., Args...>
@@ -27,64 +27,58 @@ namespace atma::detail::_functor_list_
 	};
 }
 
-namespace atma::detail::_functor_list_
+namespace atma::detail::_functor_cascade_
 {
 	template <typename Fwds, typename Gs, typename Rs>
-	struct functor_chain_t
+	struct link_t
 	{};
 
 	template <typename Fwds, typename... Gs, typename F, typename... Rs>
-	struct functor_chain_t<Fwds, std::tuple<Gs...>, std::tuple<F, Rs...>>
-		: functor_chain_t<Fwds, std::tuple<Gs..., F>, std::tuple<Rs...>>
-		, functor_delegate_t<Fwds, std::tuple<Gs...>, F>
+	struct link_t<Fwds, std::tuple<Gs...>, std::tuple<F, Rs...>>
+		: link_t<Fwds, std::tuple<Gs..., F>, std::tuple<Rs...>>
+		, dispatcher_t<Fwds, std::tuple<Gs...>, F>
 	{};
 }
 
-namespace atma::detail::_functor_list_
+namespace atma::detail::_functor_cascade_
 {
 	template <typename Fwds, typename Fs>
-	struct chain_selector_t;
-
-	template <typename Fs>
-	struct chain_selector_t<functor_list_fwds_t<>, Fs>
-		: functor_chain_t<functor_list_fwds_t<>, std::tuple<>, Fs>
+	struct chain_selector_t
+		: link_t<functor_cascade_fwds_t<>, std::tuple<>, Fs>
 	{};
 
 	template <typename... Fwds, typename... Fs>
-	struct chain_selector_t<functor_list_fwds_t<Fwds...>, std::tuple<Fs...>>
-		: functor_chain_t<functor_list_fwds_t<Fwds...>, std::tuple<>, std::tuple<Fs...>>
+	struct chain_selector_t<functor_cascade_fwds_t<Fwds...>, std::tuple<Fs...>>
+		: link_t<functor_cascade_fwds_t<Fwds...>, std::tuple<>, std::tuple<Fs...>>
 	{};
 }
 
 namespace atma
 {
-	template <typename Fwds, typename... Fs>
-	struct functor_list_t;
-
 	template <typename... Fs>
-	struct functor_list_t<Fs...>
-		: functor_list_t<functor_list_fwds_t<>, Fs...>
+	struct functor_cascade_t
+		: functor_cascade_t<functor_cascade_fwds_t<>, Fs...>
 	{};
 
 	template <typename... Fwds, typename... Fs>
-	struct functor_list_t<functor_list_fwds_t<Fwds...>, Fs...>
-		: detail::_functor_list_::chain_selector_t<functor_list_fwds_t<Fwds...>, std::tuple<Fs...>>
+	struct functor_cascade_t<functor_cascade_fwds_t<Fwds...>, Fs...>
+		: detail::_functor_cascade_::chain_selector_t<functor_cascade_fwds_t<Fwds...>, std::tuple<Fs...>>
 	{
 		static_assert((std::is_empty_v<Fwds> && ...),
 			"all forwarded types must be empty");
 
 		static_assert((std::is_empty_v<Fs> && ...),
-			"all forwarded functors must be empty");
+			"all functors must be empty");
 
-		constexpr functor_list_t(auto&&...) noexcept
+		constexpr functor_cascade_t(auto&&...) noexcept
 		{}
 	};
 
 	template <typename... Fwds, typename... Fs>
-	functor_list_t(functor_list_fwds_t<Fwds...>, Fs&&...)
-		-> functor_list_t<functor_list_fwds_t<Fwds...>, std::remove_reference_t<Fs>...>;
-
+	functor_cascade_t(functor_cascade_fwds_t<Fwds...>, Fs...)
+		-> functor_cascade_t<functor_cascade_fwds_t<Fwds...>, Fs...>;
+	
 	template <typename... Fs>
-	functor_list_t(Fs&&...)
-		-> functor_list_t<functor_list_fwds_t<>, std::remove_reference_t<Fs>...>;
+	functor_cascade_t(Fs...)
+		-> functor_cascade_t<functor_cascade_fwds_t<>, Fs...>;
 }
